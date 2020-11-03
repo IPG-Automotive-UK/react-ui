db3794dd16a28602357fc6d9593a3efe3c9ef26b
Matthew Corner <matthew.j.corner@gmail.com>
2020-11-03 14:55:20 +0000

    Add theme provider to export and storybook

---
+++ src/ThemeProvider/ThemeProvider.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
+
+// custom material-ui theme
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: "#005FA8" },
+    secondary: { main: "#003063" },
+    action: {
+      selected: "rgba(0, 95, 168, 0.08)"
+    }
+  },
+  overrides: {
+    MuiTooltip: {
+      tooltip: {
+        fontSize: "14px",
+        fontWeight: "normal"
+      }
+    },
+    MuiToggleButton: {
+      root: {
+        "&$selected": {
+          backgroundColor: "rgba(0, 95, 168, 0.08)",
+          "&:hover": {
+            backgroundColor: "rgba(0, 95, 168, 0.06)"
+          }
+        },
+        "&:hover": {
+          backgroundColor: "rgba(0, 95, 168, 0.04)"
+        }
+      }
+    }
+  }
+});
+
+/**
+ * IPG Material-ui theme provider
+ */
+export default function ThemeProvider({ children }) {
+  return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>;
+}
+
+// prop types
+ThemeProvider.propTypes = {
+  children: PropTypes.node
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 export { default as Copyright } from "./Copyright";
+export { default as ThemeProvider } from "./ThemeProvider";
