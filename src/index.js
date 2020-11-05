28d10b27f63f417a45fe76364374708af5b9ed27
Matthew Corner <matthew.j.corner@gmail.com>
2020-11-05 09:49:42 +0000

    Add missing exports

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 export { default as Copyright } from "./Copyright";
 export { default as ThemeProvider } from "./ThemeProvider";
+export { default as LoginForm } from "./LoginForm";
+export { default as RegistrationForm } from "./RegistrationForm";
