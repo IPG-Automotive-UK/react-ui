383665cd55f0ca2a1ff8032b7da729cdd6c0656a
Matthew Corner <matthew.j.corner@gmail.com>
2020-11-10 15:18:48 +0000

    Add select and alphabetise

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as Copyright } from "./Copyright";
-export { default as ThemeProvider } from "./ThemeProvider";
 export { default as LoginForm } from "./LoginForm";
 export { default as RegistrationForm } from "./RegistrationForm";
+export { default as Select } from "./Select";
+export { default as ThemeProvider } from "./ThemeProvider";
