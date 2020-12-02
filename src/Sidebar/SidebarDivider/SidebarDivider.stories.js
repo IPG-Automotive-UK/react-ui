2fbc1dace65e2360715ff912cd2552f19cc6f94f
Matthew Corner <matthew.j.corner@gmail.com>
2020-12-02 09:13:27 +0000

    Add a sidebar divider component

---
+++ src/Sidebar/SidebarDivider/SidebarDivider.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SidebarDivider from "./SidebarDivider";
+
+export default {
+  component: SidebarDivider,
+  title: "Layout/SidebarDivider"
+};
+
+const Template = () => {
+  return <SidebarDivider />;
+};
+
+export const Default = Template.bind({});
+Default.args = {};
