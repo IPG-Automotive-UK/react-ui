c3a574c7eeb252af6af077f5da08be043c07de39
David Barry <david.barry@ipg-automotive.com>
2020-12-16 14:01:44 +0000

    fix async/await pattern

--- src/LoginForm/LoginForm.test.js
+++ src/LoginForm/LoginForm.test.js
@@ -1,4 +1,10 @@
-import { act, fireEvent, render, screen } from "@testing-library/react";
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor
+} from "@testing-library/react";
 import LoginForm from "./";
 import React from "react";
 import userEvent from "@testing-library/user-event";
@@ -24,15 +30,15 @@ describe("LoginForm", () => {
   it("returns form information to callback when successfully validated", async () => {
     const onLogin = jest.fn(data => data);
     const elements = setup({ onLogin });
-    await act(async () => {
-      userEvent.type(elements.inputs.email, "joe.bloggs@domain.com");
-      userEvent.type(elements.inputs.password, "indigo shark wallplug");
-      fireEvent.submit(elements.submit);
-    });
-    expect(onLogin).toHaveReturnedWith({
-      email: "joe.bloggs@domain.com",
-      password: "indigo shark wallplug"
-    });
+    userEvent.type(elements.inputs.email, "joe.bloggs@domain.com");
+    userEvent.type(elements.inputs.password, "indigo shark wallplug");
+    fireEvent.submit(elements.submit);
+    await waitFor(() =>
+      expect(onLogin).toHaveReturnedWith({
+        email: "joe.bloggs@domain.com",
+        password: "indigo shark wallplug"
+      })
+    );
   });
   it("doesnt call callback on validation errors", async () => {
     const onLogin = jest.fn();
