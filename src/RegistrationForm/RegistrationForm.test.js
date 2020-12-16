33627acb9dd077da827dfb41a9951949b174a1ef
David Barry <david.barry@ipg-automotive.com>
2020-12-16 18:01:08 +0000

    ensure userevent is wrapped in act and fix tests giving false passes

--- src/RegistrationForm/RegistrationForm.test.js
+++ src/RegistrationForm/RegistrationForm.test.js
@@ -1,4 +1,10 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor
+} from "@testing-library/react";
 import React from "react";
 import RegistrationForm from "./";
 import { selectMaterialUiSelectOption } from "../testUtils";
@@ -34,11 +40,13 @@ describe("RegistrationForm", () => {
   it("returns form information to callback when successfully validated", async () => {
     const onRegister = jest.fn(data => data);
     const elements = setup({ onRegister });
-    userEvent.type(elements.inputs.firstName, "Joe");
-    userEvent.type(elements.inputs.lastName, "Bloggs");
-    userEvent.type(elements.inputs.email, "joe.bloggs@domain.com");
-    userEvent.type(elements.inputs.password, "indigo shark wallplug");
-    userEvent.type(elements.inputs.passwordRepeat, "indigo shark wallplug");
+    act(() => {
+      userEvent.type(elements.inputs.firstName, "Joe");
+      userEvent.type(elements.inputs.lastName, "Bloggs");
+      userEvent.type(elements.inputs.email, "joe.bloggs@domain.com");
+      userEvent.type(elements.inputs.password, "indigo shark wallplug");
+      userEvent.type(elements.inputs.passwordRepeat, "indigo shark wallplug");
+    });
     await selectMaterialUiSelectOption(elements.inputs.team, teams[0]);
     fireEvent.submit(elements.submit);
     await waitFor(() =>
@@ -55,30 +63,42 @@ describe("RegistrationForm", () => {
   it("doesnt call callback on validation errors", async () => {
     const onRegister = jest.fn();
     const elements = setup({ onRegister });
-    userEvent.type(elements.inputs.email, "joe.bloggs");
+    act(() => {
+      userEvent.type(elements.inputs.email, "joe.bloggs");
+    });
     // incorrect email format
     // missing first, lastname, team, password + password repeat
     fireEvent.submit(elements.submit);
     await waitFor(() => expect(onRegister).not.toHaveBeenCalled());
   });
   describe("Password restrictions", () => {
-    it("displays password complexity score", () => {
-      render(<RegistrationForm teams={teams} onRegister={() => {}} />);
-      const input = screen.getByLabelText("password");
-      const password = "something";
-      fireEvent.change(input, { target: { value: password } });
-      expect(
-        screen.findByText("Password strength: 0/4. Minimum required 3+.")
-      ).toBeTruthy();
+    it("displays password complexity score", async () => {
+      const onRegister = jest.fn();
+      const elements = setup({ onRegister });
+      act(() => {
+        userEvent.type(elements.inputs.password, "something");
+        userEvent.click(elements.inputs.passwordRepeat); // moving to next form element triggers validation
+      });
+      await waitFor(() =>
+        expect(
+          screen.queryByText("Password strength: 0/4. Minimum required 3+.")
+        ).toBeInTheDocument()
+      );
     });
-    it("displays user feedback on password", () => {
-      render(<RegistrationForm teams={teams} onRegister={() => {}} />);
-      const input = screen.getByLabelText("password");
-      const password = "something";
-      fireEvent.change(input, { target: { value: password } });
-      expect(
-        screen.findByText("Add another word or two. Uncommon words are better.")
-      ).toBeTruthy();
+    it("displays user feedback on password", async () => {
+      const onRegister = jest.fn();
+      const elements = setup({ onRegister });
+      act(() => {
+        userEvent.type(elements.inputs.password, "something");
+        userEvent.click(elements.inputs.passwordRepeat); // moving to next form element triggers validation
+      });
+      await waitFor(() =>
+        expect(
+          screen.queryByText(
+            "Add another word or two. Uncommon words are better."
+          )
+        ).toBeInTheDocument()
+      );
     });
   });
 });
