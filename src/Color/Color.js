6ea4d7bc66992783c51db693ef147f2a11b88a1f
jegasriskantha <75164177+jegasriskantha@users.noreply.github.com>
2021-11-13 11:20:43 +0000

    Implemented color picker with popover

--- src/Color/Color.js
+++ src/Color/Color.js
@@ -1,5 +1,6 @@
-import { Box } from "@mui/material";
-import React from "react";
+import { Box, Popover } from "@mui/material";
+import PropTypes from "prop-types";
+import React, { useEffect } from "react";
 import { RgbaColorPicker } from "react-colorful";
 
 // styling
@@ -8,51 +9,66 @@ const sx = {
     cursor: "pointer"
   },
   colorPicker: {
-    // "& .react-colorful": {
-    //   width: "100%"
-    // },
-    // "& .react-colorful__alpha": {
-    //   "border-radius": "1 0 0 0"
-    // },
-    // "& .react-colorful__alpha-pointer": {
-    //   "border-radius": "4 4 0 0",
-    //   height: "20px",
-    //   width: "20px"
-    // },
-    // "& .react-colorful__hue-pointer": {
-    //   "border-radius": "4 4 0 0",
-    //   height: "20px",
-    //   width: "20px"
-    // },
-    // "& .react-colorful__last-control": {
-    //   "border-radius": "0 0 0 0"
-    // },
-    // "& .react-colorful__saturation": {
-    //   "border-radius": "0 0 0 0"
-    // },
-    // "& .react-colorful__saturation-pointer": {
-    //   "border-radius": "4 4 0 0",
-    //   height: "20px",
-    //   width: "20px"
-    // }
+    "& .react-colorful": {
+      width: "100%"
+    },
+    "& .react-colorful__alpha": {
+      "border-radius": "1 0 0 0"
+    },
+    "& .react-colorful__alpha-pointer": {
+      "border-radius": "4 4 0 0",
+      height: "15px",
+      width: "15px"
+    },
+    "& .react-colorful__hue-pointer": {
+      "border-radius": "4 4 0 0",
+      height: "15px",
+      width: "15px"
+    },
+    "& .react-colorful__last-control": {
+      "border-radius": "0 0 0 0"
+    },
+    "& .react-colorful__saturation": {
+      "border-radius": "0  0 0"
+    },
+    "& .react-colorful__saturation-pointer": {
+      "border-radius": "4 4 0 0",
+      height: "15px",
+      width: "15px"
+    }
   }
 };
 
-export default function Color(props) {
-  // create colour picker open state
-  const [open, setOpen] = React.useState(false);
-
+export default function Color({
+  height = "15px",
+  width = "30px",
+  value = "rgba(255,0,0,1)",
+  onChange = () => {}
+}) {
   // create color states
-  const [color, setColor] = React.useState("rgba(255,0,0,1)");
+  const [color, setColor] = React.useState(value);
 
-  // handle onClick
-  const handleClick = () => {
-    setOpen(!open);
-  };
+  // set color everytime color is changed
+  useEffect(() => {
+    setColor(value);
+  }, [value]);
 
   const handleChange = color => {
-    setColor(`rgba(${color.r},${color.g},${color.b},${color.a})`);
+    onChange(setColor(`rgba(${color.r},${color.g},${color.b},${color.a})`));
   };
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
 
   // convert color string to rgba object
   const rgba = color.substring(5, color.length - 1).split(",");
@@ -64,28 +80,52 @@ export default function Color(props) {
   };
 
   return (
-    <div>
-      <div onClick={handleClick}>
+    <Box>
+      <Box onClick={handleClick}>
         <Box
           sx={
             (sx.colorBox,
             {
               background: color,
-              height: props.height || "14px",
-              width: props.width || "36px"
+              height: height,
+              width: width
             })
           }
         />
-      </div>
-      {open && (
-        <div sx={{ position: "absolute", zIndex: 2 }}>
-          <RgbaColorPicker
-            sx={sx.colorPicker}
-            color={rgbaObj}
-            onChange={handleChange}
-          />
-        </div>
-      )}
-    </div>
+      </Box>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left"
+        }}
+      >
+        <Box sx={{ display: "flex" }}>
+          <Box sx={{ width: "200px", height: "200px", m: 1 }}>
+            <Box sx={sx.colorPicker}>
+              <RgbaColorPicker color={rgbaObj} onChange={handleChange} />
+            </Box>
+          </Box>
+        </Box>
+      </Popover>
+    </Box>
   );
 }
+
+Color.propTypes = {
+  /**
+   * rgba color string
+   */
+  value: PropTypes.string,
+  /**
+   * rgba color string
+   */
+  height: PropTypes.string,
+  /**
+   * rgba color string
+   */
+  width: PropTypes.string
+};
--- src/Color/Color.stories.js
+++ src/Color/Color.stories.js
@@ -2,13 +2,38 @@ import Color from "./Color";
 import React from "react";
 
 export default {
+  argTypes: {
+    value: {
+      control: { type: "color" }
+    }
+  },
   component: Color,
   title: "General/Color"
 };
 
-const Template = () => {
-  return <Color />;
+const Template = args => {
+  const [value, setValue] = React.useState(args.value);
+  React.useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  const onChange = color => setValue(color);
+
+  console.log(args.value);
+  console.log(value);
+
+  return (
+    <Color
+      value={value}
+      onChange={onChange}
+      height={args.height}
+      width={args.width}
+    />
+  );
 };
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  value: "rgba(255,0,0,1)",
+  height: "15px",
+  width: "30px"
+};
