ba82b9ae40043ce3df090cb098edbbc3a587ebae
jegasriskantha <75164177+jegasriskantha@users.noreply.github.com>
2021-11-14 20:25:49 +0000

    Updated Red, Green, Blue and Alpha  textfields to controlled states

--- src/Color/Color.js
+++ src/Color/Color.js
@@ -1,4 +1,4 @@
-import { Box, Popover } from "@mui/material";
+import { Box, Popover, TextField } from "@mui/material";
 import PropTypes from "prop-types";
 import React, { useEffect } from "react";
 import { RgbaColorPicker } from "react-colorful";
@@ -48,25 +48,78 @@ export default function Color({
   // create color states
   const [color, setColor] = React.useState(value);
 
+  // create popover states
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
   // set color everytime color is changed
   useEffect(() => {
     setColor(value);
   }, [value]);
 
+  // handle color change
   const handleChange = color => {
     onChange(setColor(`rgba(${color.r},${color.g},${color.b},${color.a})`));
   };
 
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  // handle red change
+  const handleRedChange = event => {
+    const rgba = color.substring(5, color.length - 1).split(",");
+    onChange(
+      setColor(
+        `rgba(${event.target.value},${Number(rgba[1])},${Number(
+          rgba[2]
+        )},${Number(rgba[3])})`
+      )
+    );
+  };
 
+  // handle green change
+  const handleGreenChange = event => {
+    const rgba = color.substring(5, color.length - 1).split(",");
+    onChange(
+      setColor(
+        `rgba(${Number(rgba[0])},${event.target.value},${Number(
+          rgba[2]
+        )},${Number(rgba[3])})`
+      )
+    );
+  };
+
+  // handle Blue change
+  const handleBlueChange = event => {
+    const rgba = color.substring(5, color.length - 1).split(",");
+    onChange(
+      setColor(
+        `rgba(${Number(rgba[0])},${Number(rgba[1])},${
+          event.target.value
+        },${Number(rgba[3])})`
+      )
+    );
+  };
+
+  // handle alpha change
+  const handleAlphaChange = event => {
+    const rgba = color.substring(5, color.length - 1).split(",");
+    onChange(
+      setColor(
+        `rgba(${Number(rgba[0])},${Number(rgba[1])},${Number(rgba[2])},${
+          event.target.value
+        })`
+      )
+    );
+  };
+
+  // handle popover open
   const handleClick = event => {
     setAnchorEl(event.currentTarget);
   };
 
+  // handle popover close
   const handleClose = () => {
     setAnchorEl(null);
   };
 
+  // open and id states
   const open = Boolean(anchorEl);
   const id = open ? "simple-popover" : undefined;
 
@@ -103,11 +156,71 @@ export default function Color({
           horizontal: "left"
         }}
       >
-        <Box sx={{ display: "flex" }}>
-          <Box sx={{ width: "200px", height: "200px", m: 1 }}>
-            <Box sx={sx.colorPicker}>
-              <RgbaColorPicker color={rgbaObj} onChange={handleChange} />
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <Box sx={{ width: "250px", m: 1 }}>
+            <Box sx={{ width: "250px" }}>
+              <Box sx={sx.colorPicker}>
+                <RgbaColorPicker color={rgbaObj} onChange={handleChange} />
+              </Box>
             </Box>
+            <Box sx={{ mt: 1, display: "flex", flexDirection: "row" }}>
+              <TextField
+                id="outlined-number"
+                type="number"
+                size="small"
+                margin="dense"
+                label="Red"
+                value={rgbaObj.r}
+                sx={{ width: "33%" }}
+                InputLabelProps={{
+                  shrink: true
+                }}
+                onChange={handleRedChange}
+                inputProps={{ min: 0, max: 255 }}
+              />
+              <TextField
+                id="outlined-number"
+                type="number"
+                size="small"
+                margin="dense"
+                label="Green"
+                value={rgbaObj.g}
+                sx={{ width: "33%" }}
+                InputLabelProps={{
+                  shrink: true
+                }}
+                onChange={handleGreenChange}
+                inputProps={{ min: 0, max: 255 }}
+              />
+              <TextField
+                id="outlined-number"
+                type="number"
+                size="small"
+                margin="dense"
+                label="Blue"
+                value={rgbaObj.b}
+                sx={{ width: "33%" }}
+                InputLabelProps={{
+                  shrink: true
+                }}
+                onChange={handleBlueChange}
+                inputProps={{ min: 0, max: 255 }}
+              />
+            </Box>
+            <TextField
+              id="outlined-number"
+              type="number"
+              size="small"
+              margin="dense"
+              label="Alpha (Transparency)"
+              value={rgbaObj.a}
+              fullWidth
+              InputLabelProps={{
+                shrink: true
+              }}
+              onChange={handleAlphaChange}
+              inputProps={{ min: 0, max: 1, step: 0.1 }}
+            />
           </Box>
         </Box>
       </Popover>
