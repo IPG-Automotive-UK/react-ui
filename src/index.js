3b23e509b9e997ea96fa4184586f14b65a6bb399
evoinea <89025096+evoinea@users.noreply.github.com>
2021-11-16 11:29:19 +0000

    Added Slider to index

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ export { default as RegistrationForm } from "./RegistrationForm";
 export { default as SearchBar } from "./SearchBar";
 export { default as Select } from "./Select";
 export { Sidebar, SidebarDivider, SidebarItem } from "./Sidebar";
+export { default as Slider } from "./Slider";
 export { default as Snackbar } from "./Snackbar";
 export { SnackbarProvider, useSnackbar } from "./SnackbarProvider";
 export { default as SwitchField } from "./SwitchField";
