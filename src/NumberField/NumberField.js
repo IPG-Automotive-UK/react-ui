b19aff993388343ee45dc653b89a43489230d918
jegasriskantha <75164177+jegasriskantha@users.noreply.github.com>
2021-11-17 14:47:48 +0000

    Added NumberField

---
+++ src/NumberField/NumberField.js
@@ -0,0 +1,105 @@
+import { TextField as MuiTextField } from "@mui/material";
+import PropTypes from "prop-types";
+import React from "react";
+
+/**
+ * NumberField components are used for collecting user provided information as a Number.
+ */
+export default function NumberField({
+  disabled = false,
+  error = false,
+  helperText,
+  label,
+  margin = "normal",
+  onChange = () => {},
+  placeholder,
+  required = false,
+  size = "medium",
+  value,
+  variant = "outlined"
+}) {
+  // handleChange
+  const handleChange = event => {
+    // update event.target.value so that it is a number
+    const newEventValue = { target: { value: Number(event.target.value) } };
+    const updatedEvent = { ...event, ...newEventValue };
+    onChange(updatedEvent);
+  };
+  // return components
+  return (
+    <MuiTextField
+      data-testid="NumberField"
+      disabled={disabled}
+      error={error}
+      helperText={helperText}
+      label={label}
+      margin={margin}
+      onChange={handleChange}
+      placeholder={placeholder}
+      required={required}
+      size={size}
+      type="Number"
+      value={value}
+      variant={variant}
+    />
+  );
+}
+
+NumberField.propTypes = {
+  /**
+   * If true, the label, input and helper text should be displayed in a disabled state.
+   * @default false
+   */
+  disabled: PropTypes.bool,
+  /**
+   * If true, the component will display an error state.
+   * @default false
+   */
+  error: PropTypes.bool,
+  /**
+   * Helper text to display below input.
+   */
+  helperText: PropTypes.string,
+  /**
+   * Label to display above input.
+   */
+  label: PropTypes.string,
+  /**
+   * If dense or normal, will adjust vertical spacing of this and contained components.
+   * @default "normal"
+   */
+  margin: PropTypes.oneOf(["none", "dense", "normal"]),
+  /**
+   * Callback fired when the value is changed.
+   *
+   * **Signature**
+   * ```
+   * function(event: object) => void
+   * ```
+   * _event_: The event source of the callback. You can pull out the new value by accessing `event.target.value` (number).
+   */
+  onChange: PropTypes.func,
+  /**
+   * Placeholder text to display when there is no value.
+   */
+  placeholder: PropTypes.number,
+  /**
+   * If true, the label will indicate that the input is required.
+   * @default false
+   */
+  required: PropTypes.bool,
+  /**
+   * The size of the select field.
+   * @default "medium"
+   */
+  size: PropTypes.oneOf(["medium", "small"]),
+  /**
+   * The input value
+   */
+  value: PropTypes.number,
+  /**
+   * The variant to use.
+   * @default "outlined"
+   */
+  variant: PropTypes.oneOf(["standard", "outlined", "filled"])
+};
---
+++ src/NumberField/NumberField.stories.js
@@ -0,0 +1,32 @@
+import React from "react";
+import NumberField from "./NumberField";
+import { action } from "@storybook/addon-actions";
+
+export default {
+  component: NumberField,
+  title: "General/NumberField"
+};
+
+const Template = args => {
+  const [value, setValue] = React.useState(args.value);
+  React.useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  const onChange = event => {
+    setValue(value);
+    action("onChange")(event, event.target.value);
+  };
+  return <NumberField {...args} onChange={onChange} value={value} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  disabled: false,
+  error: false,
+  helperText: "What Number are you going to type?",
+  label: "Enter some a Number",
+  margin: "normal",
+  required: false,
+  size: "medium",
+  variant: "outlined"
+};
