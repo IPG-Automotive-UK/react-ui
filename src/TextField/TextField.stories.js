321d63e9057bae0f8f0e0861da44915c099b56a0
jegasriskantha <75164177+jegasriskantha@users.noreply.github.com>
2021-11-17 13:28:23 +0000

    Created TextField Component

---
+++ src/TextField/TextField.stories.js
@@ -0,0 +1,32 @@
+import React from "react";
+import TextField from "./TextField";
+import { action } from "@storybook/addon-actions";
+
+export default {
+  component: TextField,
+  title: "General/TextField"
+};
+
+const Template = args => {
+  const [value, setValue] = React.useState(args.value);
+  React.useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  const onChange = event => {
+    setValue(value);
+    action("onChange")(event, event.target.value);
+  };
+  return <TextField {...args} onChange={onChange} value={value} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  disabled: false,
+  error: false,
+  helperText: "What are you going to type?",
+  label: "Enter some text here",
+  margin: "normal",
+  required: false,
+  size: "medium",
+  variant: "outlined"
+};
