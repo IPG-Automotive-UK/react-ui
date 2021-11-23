23ca801f5af69f88d0412603b273a9b526f9faef
David Barry <david.barry@ipg-automotive.com>
2021-11-23 14:25:09 +0000

    new font picker component

---
+++ src/FontPicker/FontPicker.stories.js
@@ -0,0 +1,48 @@
+import FontPicker from "./FontPicker";
+import React from "react";
+import { action } from "@storybook/addon-actions";
+
+export default {
+  argTypes: {
+    value: { type: "string" }
+  },
+  component: FontPicker,
+  title: "General/FontPicker"
+};
+
+const Template = args => {
+  const [value, setValue] = React.useState(args.value);
+  React.useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  const onChange = (event, value, reason) => {
+    setValue(value);
+    action("onChange")(event, value, reason);
+  };
+  return <FontPicker {...args} onChange={onChange} value={value} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  disabled: false,
+  error: false,
+  label: "Font",
+  margin: "normal",
+  required: false,
+  size: "medium",
+  value: "Arial",
+  variant: "outlined"
+};
+
+export const CustomOptions = Template.bind({});
+CustomOptions.args = {
+  disabled: false,
+  error: false,
+  label: "Font",
+  margin: "normal",
+  options: ["Arial", "Helvetica", "Times New Roman"],
+  required: false,
+  size: "medium",
+  value: "Arial",
+  variant: "outlined"
+};
