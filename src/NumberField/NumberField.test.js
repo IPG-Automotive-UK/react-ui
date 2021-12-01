19216adce44468262a329150c187f58b4266b1fa
jegasriskantha <75164177+jegasriskantha@users.noreply.github.com>
2021-12-01 14:35:10 +0000

    fixed imports

--- src/NumberField/NumberField.test.js
+++ src/NumberField/NumberField.test.js
@@ -1,6 +1,6 @@
-import { fireEvent, render } from "@testing-library/react";
 import NumberField from ".";
 import React from "react";
+import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 /**
