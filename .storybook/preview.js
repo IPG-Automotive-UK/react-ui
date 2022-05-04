643b061ac0f622653082aa07fd6a131723161916
evoinea <89025096+evoinea@users.noreply.github.com>
2022-05-04 11:12:52 +0100

    Updated react ui to use React18

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { addDecorator } from "@storybook/react";
-import ThemeProvider from "../src/ThemeProvider/ThemeProvider";
-
-addDecorator(story => <ThemeProvider>{story()}</ThemeProvider>);
-
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" }
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/
+    }
+  }
 };
