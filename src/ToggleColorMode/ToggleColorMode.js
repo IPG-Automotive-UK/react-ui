93d369ca1fb933683a9e40bd09a61a5c51a30fe7
nikitharani <nikithanalumachu@gmail.com>
2022-06-08 12:47:18 +0100

    changed button to switch and added tests

--- src/ToggleColorMode/ToggleColorMode.js
+++ src/ToggleColorMode/ToggleColorMode.js
@@ -1,35 +1,73 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Brightness4Icon from "@mui/icons-material/Brightness4";
-import Brightness7Icon from "@mui/icons-material/Brightness7";
-import IconButton from "@mui/material/IconButton";
+import { Box } from "@mui/system";
 import PropTypes from "prop-types";
+import Switch from "@mui/material/Switch";
+import { styled } from "@mui/material/styles";
 
 export default function ToggleColorMode({ mode, onChange }) {
-  // handle button click
-  const handleClick = () => {
+  // handle switch change
+  const handleChange = event => {
     const newMode = mode === "dark" ? "light" : "dark";
     onChange(newMode);
   };
 
-  // define components
-  return (
-    <Box
-      sx={{
-        alignItems: "center",
-        bgcolor: "background.default",
-        borderRadius: 1,
-        color: "text.primary",
-        display: "flex",
-        justifyContent: "center",
-        p: 3,
+  // customise switch
+  const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+    "& .MuiSwitch-switchBase": {
+      "&.Mui-checked": {
+        "& + .MuiSwitch-track": {
+          backgroundColor:
+            theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+          opacity: 1
+        },
+        "& .MuiSwitch-thumb:before": {
+          backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+            "#fff"
+          )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`
+        },
+        color: "#fff",
+        transform: "translateX(22px)"
+      },
+      margin: 1,
+      padding: 0,
+      transform: "translateX(6px)"
+    },
+    "& .MuiSwitch-thumb": {
+      "&:before": {
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          "#fff"
+        )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        content: "''",
+        height: "100%",
+        left: 0,
+        position: "absolute",
+        top: 0,
         width: "100%"
-      }}
-    >
-      {mode} mode
-      <IconButton sx={{ ml: 1 }} onClick={handleClick} color="inherit">
-        {mode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
-      </IconButton>
+      },
+      backgroundColor: theme.palette.mode === "dark" ? "#003892" : "#001e3c",
+      height: 32,
+      width: 32
+    },
+    "& .MuiSwitch-track": {
+      backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+      borderRadius: 20 / 2,
+      opacity: 1
+    },
+    height: 34,
+    padding: 7,
+    width: 62
+  }));
+
+  // Switch to change mode
+  return (
+    <Box>
+      <MaterialUISwitch
+        sx={{ m: 1 }}
+        checked={mode === "dark"}
+        onChange={handleChange}
+      />
     </Box>
   );
 }
--- src/ToggleColorMode/ToggleColorMode.test.js
+++ src/ToggleColorMode/ToggleColorMode.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import ToggleColorMode from ".";
-import fireEvent from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 
 /**
  * Test wrapper for SwitchMode
@@ -17,43 +17,37 @@ const ColorModeWithState = ({ onChange, mode: modeIn = "light", ...rest }) => {
 };
 
 describe("ToggleColorMode", () => {
-  test("Brightness7Icon is shown for dark mode", () => {
-    render(<ColorModeWithState mode="dark" />);
-
-    // verify dark mode icon is shown
-    const darkmode = screen.getByTestId("Brightness7Icon");
-    expect(darkmode).toBeInTheDocument();
-  });
-
-  test("Brightness4Icon is shown for light mode", () => {
+  test("In light mode switch is unchecked", () => {
     render(<ColorModeWithState mode="light" />);
 
-    // verify light mode icon is shown
-    const lightmode = screen.getByTestId("Brightness4Icon");
-    expect(lightmode).toBeInTheDocument();
+    const checkbox = screen.getByRole("checkbox");
+    expect(checkbox.checked).toBeFalsy();
+  });
+
+  test("In dark mode switch is checked", () => {
+    render(<ColorModeWithState mode="dark" />);
+
+    const checkbox = screen.getByRole("checkbox");
+    expect(checkbox.checked).toBeTruthy();
   });
 
   test("onClick switch change from light to dark mode", () => {
     const onChange = jest.fn();
 
-    render(<ColorModeWithState onChange={onChange} />);
-    const button = screen.getByRole("button");
+    render(<ColorModeWithState onChange={onChange} mode="light" />);
+    const button = screen.getByRole("checkbox");
 
-    fireEvent.click(button);
-    expect(document.querySelector("#sandbox > svg > path"));
-
-    fireEvent.click(button);
-    expect(screen.getByTestId("Brightness4Icon"));
+    userEvent.click(button);
+    expect(onChange).toHaveBeenCalledWith("dark");
   });
 
   test("onClick switch change from dark to light mode", () => {
-    render(<ColorModeWithState mode="dark" />);
-    const button = screen.getByRole("button");
+    const onChange = jest.fn();
 
-    fireEvent.click(button);
-    expect(screen.getByTestId("Brightness4Icon"));
+    render(<ColorModeWithState onChange={onChange} mode="dark" />);
+    const button = screen.getByRole("checkbox");
 
-    fireEvent.click(button);
-    expect(document.querySelector("#sandbox > svg > path"));
+    userEvent.click(button);
+    expect(onChange).toHaveBeenCalledWith("light");
   });
 });
