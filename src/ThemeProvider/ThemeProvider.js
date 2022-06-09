69c59d577623220b610641c1617ac079956264a2
nikitharani <nikithanalumachu@gmail.com>
2022-06-09 12:46:38 +0100

    added dark theme

--- src/ThemeProvider/ThemeProvider.js
+++ src/ThemeProvider/ThemeProvider.js
@@ -6,7 +6,7 @@ import PropTypes from "prop-types";
 import React from "react";
 
 // custom material-ui theme
-const theme = createTheme({
+const LightTheme = createTheme({
   overrides: {
     MuiAccordionSummary: {
       root: {
@@ -51,11 +51,61 @@ const theme = createTheme({
   }
 });
 
+// custom theme for dark mode
+const DarkTheme = createTheme({
+  overrides: {
+    MuiAccordionSummary: {
+      root: {
+        "&$expanded": { marginBottom: -20 }
+      }
+    },
+    MuiIconButton: { root: { color: "#9e9e9e" } },
+    MuiTableRow: {
+      root: {
+        "&$selected": {
+          backgroundColor: "rgba(0, 95, 168, 0.08)"
+        }
+      }
+    },
+    MuiToggleButton: {
+      root: {
+        "&$selected": {
+          "&:hover": {
+            backgroundColor: "rgba(0, 95, 168, 0.15)"
+          },
+          backgroundColor: "rgba(0, 95, 168, 0.08)"
+        },
+        "&:hover": {
+          backgroundColor: "rgba(0, 95, 168, 0.15)"
+        },
+        "border-color": "rgb(196, 196, 196)"
+      }
+    },
+    MuiTooltip: {
+      tooltip: {
+        fontSize: "14px",
+        fontWeight: "normal"
+      }
+    }
+  },
+  palette: {
+    action: {
+      selected: "rgba(0, 95, 168, 0.08)"
+    },
+    primary: { main: "#ced5db" },
+    secondary: { main: "#8bacc4" }
+  }
+});
+
 /**
  * IPG Material-ui theme provider
  */
-export default function ThemeProvider({ children }) {
-  return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>;
+export default function ThemeProvider({ children, mode }) {
+  return (
+    <MuiThemeProvider theme={mode === "light" ? LightTheme : DarkTheme}>
+      {children}
+    </MuiThemeProvider>
+  );
 }
 
 // prop types
