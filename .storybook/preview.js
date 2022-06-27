55bc4ba1ca062fbc3985d41e61556666f36db8b7
evoinea <89025096+evoinea@users.noreply.github.com>
2022-06-27 14:47:07 +0100

    Added back ThemeProvider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,9 @@
+import React from "react";
+import { addDecorator } from "@storybook/react";
+import ThemeProvider from "../src/ThemeProvider/ThemeProvider";
+
+addDecorator(story => <ThemeProvider>{story()}</ThemeProvider>);
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
