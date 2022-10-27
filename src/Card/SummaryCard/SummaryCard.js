c3ae086dde87a3640e6e27049d5a3efde3c74847
jegasriskantha <75164177+jegasriskantha@users.noreply.github.com>
2022-10-27 15:44:39 +0100

    updated to useLayoutEffect and added popovers and onClick callbacks

--- src/Card/SummaryCard/SummaryCard.js
+++ src/Card/SummaryCard/SummaryCard.js
@@ -2,16 +2,18 @@ import {
   Box,
   Button,
   Card,
+  CardActions,
   CardContent,
   CardHeader,
   CardMedia,
   Divider,
   IconButton,
+  Popover,
   Stack,
   Tooltip,
   Typography
 } from "@mui/material";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 
 import LabelChip from "../../LabelSelector/LabelChip/LabelChip";
 import { MoreVert } from "@mui/icons-material";
@@ -22,7 +24,15 @@ function SummaryCard({
   subtitle = "subtitle",
   labels = [],
   media = "",
-  content = null
+  content = null,
+  moreOptionsList = null,
+  width = 436,
+  height = 613,
+  mediaWidth = 400,
+  mediaHeight = 200,
+  onClickLabel = () => {},
+  onClickMoreDetails = () => {},
+  onClickViewFiles = () => {}
 }) {
   const titleRef = useRef();
   const subtitleRef = useRef();
@@ -31,38 +41,44 @@ function SummaryCard({
   const [isSubtitleOverflow, setIsSubtitleOverflow] = useState(false);
   const [isLabelStackOverflow, setIsLabelStackOverflow] = useState(false);
   const [overFlowingLabels, setOverFlowingLabels] = useState([]);
+  const [labelAnchorEl, setLabelAnchorEl] = useState(null);
+  const [moreOptionsAnchorEl, setMoreOptionsAnchorEl] = useState(null);
 
   // header content width
-  const headerContentWidth = 350;
+  const headerContentWidth = width - 65;
+  const labelSpacing = 8;
+
+  // overflow button width
+  const overflowButtonWidth = 40;
 
   // check if title is overflowing
-  useEffect(() => {
+  useLayoutEffect(() => {
     setIsTitleOverflow(
       titleRef.current.scrollWidth > titleRef.current.clientWidth
     );
   }, []);
 
   // check if subtitle is overflowing
-  useEffect(() => {
+  useLayoutEffect(() => {
     setIsSubtitleOverflow(
       subtitleRef.current.scrollWidth > subtitleRef.current.clientWidth
     );
   }, []);
 
   // check if label stack is overflowing
-  useEffect(() => {
+  useLayoutEffect(() => {
     setIsLabelStackOverflow(
       labelStackRef.current.scrollWidth > labelStackRef.current.clientWidth
     );
 
     // find the overflowing labels
-    let sum = 0;
+    let sum = overflowButtonWidth;
     const overFlowLabels = [];
 
     // for each of the labels in the label stack
     labelStackRef.current.childNodes.forEach(child => {
       // add the width of the child plus 8px of space between each child to the sum
-      sum += child.offsetWidth + 8;
+      sum += child.offsetWidth + labelSpacing;
 
       // if the sum is greater than the header contend width, then them this child is overflowing
       // the header content width
@@ -81,107 +97,224 @@ function SummaryCard({
     labels.length - overFlowingLabels.length
   );
 
-  // based on the not overflowing labels, determine the overflowing label names
-  const overFlowingLabelNames = overFlowingLabels.map(label => label.innerText);
+  const handleLabelOverflowClick = event => {
+    setLabelAnchorEl(event.currentTarget);
+  };
 
-  // create an div element to display the overflowing labels with a <br /> between each label
-  const OverFlowingLabelTooltip = (
-    <div>
-      {overFlowingLabelNames.map((label, index) => (
-        <React.Fragment key={index}>
-          {label}
-          {index !== overFlowingLabelNames.length - 1 && <br />}
-        </React.Fragment>
-      ))}
-    </div>
-  );
+  const handleLabelOverflowClose = () => {
+    setLabelAnchorEl(null);
+  };
+
+  const handleMoreOptionsClick = event => {
+    setMoreOptionsAnchorEl(event.currentTarget);
+  };
+
+  const handleMoreOptionsClose = () => {
+    setMoreOptionsAnchorEl(null);
+  };
+
+  // determine if the label overflow popover is open
+  const isLabelOverflowOpen = Boolean(labelAnchorEl);
+
+  // determine if the more options popover is open
+  const isMoreOptionsOpen = Boolean(moreOptionsAnchorEl);
+
+  // handle label click by calling the onClickLabel prop
+  const handleLabelClick = label => {
+    onClickLabel(label);
+  };
 
   return (
-    <Card sx={{ height: 613, width: 436 }}>
-      <CardHeader
-        sx={{ height: 51 }}
-        action={
-          <IconButton aria-label="settings">
-            <MoreVert />
-          </IconButton>
-        }
-        disableTypography
-        title={
-          <Tooltip title={title} disableHoverListener={!isTitleOverflow}>
-            <Typography
-              ref={titleRef}
-              sx={{ fontSize: 20, fontWeight: 500, width: headerContentWidth }}
-              noWrap
+    <>
+      <Card sx={{ height, width }}>
+        <CardHeader
+          sx={{ height: 50 }}
+          action={
+            moreOptionsList ? (
+              <IconButton
+                aria-label="settings"
+                onClick={handleMoreOptionsClick}
+              >
+                <MoreVert />
+              </IconButton>
+            ) : null
+          }
+          disableTypography
+          title={
+            <Tooltip title={title} disableHoverListener={!isTitleOverflow}>
+              <Typography
+                ref={titleRef}
+                sx={{
+                  fontSize: 20,
+                  fontWeight: 500,
+                  width: headerContentWidth
+                }}
+                noWrap
+              >
+                {title}
+              </Typography>
+            </Tooltip>
+          }
+          subheader={
+            <Tooltip
+              title={subtitle}
+              disableHoverListener={!isSubtitleOverflow}
             >
-              {title}
-            </Typography>
-          </Tooltip>
-        }
-        subheader={
-          <Tooltip title={subtitle} disableHoverListener={!isSubtitleOverflow}>
-            <Typography
-              ref={subtitleRef}
-              sx={{ fontSize: 14, fontWeight: 400, width: headerContentWidth }}
-              noWrap
-            >
-              {subtitle}
-            </Typography>
-          </Tooltip>
-        }
-      />
-      <Box ml={2} sx={{ maxWidth: 400, height: 24, overflowX: "hidden" }}>
-        <Stack ref={labelStackRef} direction="row" spacing={1}>
-          {!isLabelStackOverflow ? (
-            <>
-              {labels.map(label => (
-                <LabelChip
-                  key={label._id}
-                  label={label.name}
-                  color={label.color}
-                  size="small"
-                />
-              ))}
-            </>
-          ) : (
-            <>
-              {notOverflowingLabels.map(label => (
-                <LabelChip
-                  key={label._id}
-                  label={label.name}
-                  color={label.color}
-                  size="small"
-                />
-              ))}
-              <Tooltip title={OverFlowingLabelTooltip}>
-                <Typography>+{overFlowingLabels.length}</Typography>
-              </Tooltip>
-            </>
-          )}
-        </Stack>
-      </Box>
-      <CardMedia
-        component="img"
-        src={media}
-        sx={{ height: 200, objectFit: "contain", padding: 2, width: 400 }}
-      />
-      <CardContent sx={{ height: 216, overflowY: "hidden", padding: 1 }}>
-        {content}
-      </CardContent>
-      <Divider />
-      <Stack direction="row">
-        <Button size="large" variant="text" sx={{ width: "50%" }}>
-          MORE DETAILS
-        </Button>
-        <Divider
-          orientation="vertical"
-          flexItem
-          sx={{ background: theme => theme.palette.primary.main }}
+              <Typography
+                ref={subtitleRef}
+                sx={{
+                  fontSize: 14,
+                  fontWeight: 400,
+                  width: headerContentWidth
+                }}
+                noWrap
+              >
+                {subtitle}
+              </Typography>
+            </Tooltip>
+          }
         />
-        <Button size="large" variant="text" sx={{ width: "50%" }}>
-          VIEW FILES
-        </Button>
-      </Stack>
-    </Card>
+        <Box
+          ml={2}
+          sx={{ height: 24, maxWidth: headerContentWidth, overflowX: "hidden" }}
+        >
+          <Stack
+            ref={labelStackRef}
+            direction="row"
+            spacing={`${labelSpacing}px`}
+          >
+            {!isLabelStackOverflow ? (
+              <>
+                {labels.map(label => (
+                  <LabelChip
+                    key={label._id}
+                    label={label.name}
+                    color={label.color}
+                    onClick={() => handleLabelClick(label)}
+                    size="small"
+                  />
+                ))}
+              </>
+            ) : (
+              <>
+                {notOverflowingLabels.map(label => (
+                  <LabelChip
+                    clickable
+                    key={label._id}
+                    label={label.name}
+                    color={label.color}
+                    onClick={() => handleLabelClick(label)}
+                    size="small"
+                  />
+                ))}
+                <Button
+                  variant="text"
+                  size="large"
+                  onClick={handleLabelOverflowClick}
+                  mt={0.5}
+                  sx={{
+                    maxWidth: overflowButtonWidth,
+                    minWidth: overflowButtonWidth,
+                    padding: 0
+                  }}
+                >
+                  <Typography sx={{ fontSize: 15 }}>
+                    +{overFlowingLabels.length}
+                  </Typography>
+                </Button>
+              </>
+            )}
+          </Stack>
+        </Box>
+        <CardMedia
+          component="img"
+          src={media}
+          sx={{
+            height: mediaHeight,
+            objectFit: "contain",
+            padding: 2,
+            width: mediaWidth
+          }}
+        />
+        <CardContent
+          sx={{
+            height: height - mediaHeight - 196,
+            overflowY: "hidden",
+            padding: 1
+          }}
+        >
+          {content}
+        </CardContent>
+        <Divider />
+        <CardActions disableSpacing sx={{ padding: 0 }}>
+          <Button
+            size="large"
+            variant="text"
+            sx={{ width: "50%" }}
+            onClick={onClickMoreDetails}
+          >
+            MORE DETAILS
+          </Button>
+          <Divider
+            orientation="vertical"
+            flexItem
+            sx={{ background: theme => theme.palette.primary.main }}
+          />
+          <Button
+            size="large"
+            variant="text"
+            sx={{ width: "50%" }}
+            onClick={onClickViewFiles}
+          >
+            VIEW FILES
+          </Button>
+        </CardActions>
+      </Card>
+      <Popover
+        open={isLabelOverflowOpen}
+        anchorEl={labelAnchorEl}
+        onClose={handleLabelOverflowClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left"
+        }}
+      >
+        <Stack
+          sx={{}}
+          m={`${labelSpacing}px`}
+          direction="column"
+          spacing={`${labelSpacing}px`}
+        >
+          {labels.map(label => {
+            if (!notOverflowingLabels.includes(label)) {
+              return (
+                <LabelChip
+                  clickable
+                  key={label._id}
+                  label={label.name}
+                  color={label.color}
+                  size="small"
+                  onClick={() => handleLabelClick(label)}
+                />
+              );
+            } else {
+              return null;
+            }
+          })}
+        </Stack>
+      </Popover>
+      <Popover
+        open={isMoreOptionsOpen}
+        anchorEl={moreOptionsAnchorEl}
+        onClose={handleMoreOptionsClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left"
+        }}
+      >
+        {moreOptionsList}
+      </Popover>
+    </>
   );
 }
 
@@ -189,16 +322,21 @@ export default SummaryCard;
 
 // summary card prop types
 SummaryCard.propTypes = {
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
+  content: PropTypes.node.isRequired,
+  height: PropTypes.number,
   labels: PropTypes.arrayOf(
     PropTypes.shape({
       _id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
+      color: PropTypes.string.isRequired,
       description: PropTypes.string,
-      color: PropTypes.string.isRequired
+      name: PropTypes.string.isRequired
     })
   ),
   media: PropTypes.string,
-  content: PropTypes.node.isRequired
+  subtitle: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  width: PropTypes.number,
+  onClickLabel: PropTypes.func,
+  onClickMoreDetails: PropTypes.func,
+  onClickViewFiles: PropTypes.func
 };
--- src/Card/SummaryCard/SummaryCard.stories.js
+++ src/Card/SummaryCard/SummaryCard.stories.js
@@ -1,12 +1,20 @@
-import React from "react";
-import SummaryCard from "./SummaryCard";
+import { Delete, Edit } from "@mui/icons-material";
 import {
-  TableContainer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
   Table,
   TableBody,
-  TableRow,
-  TableCell
+  TableCell,
+  TableContainer,
+  TableRow
 } from "@mui/material";
+
+import React from "react";
+import SummaryCard from "./SummaryCard";
+import { action } from "@storybook/addon-actions";
 
 export default {
   component: SummaryCard,
@@ -14,46 +22,66 @@ export default {
 };
 
 const Template = args => {
-  return <SummaryCard {...args} />;
+  return (
+    <SummaryCard
+      {...args}
+      onLabelClick={label => {
+        action("onLabelClick")(label);
+      }}
+      onMoreDetailsClick={() => {
+        action("onMoreDetailsClick")();
+      }}
+      onViewFilesClick={() => {
+        action("onViewFilesClick")();
+      }}
+    />
+  );
 };
 
 export const Default = Template.bind({});
 Default.args = {
+  height: 613,
+  subtitle: "subtitle",
   title: "title",
-  subtitle: "subtitle"
+  width: 436,
+  mediaHeight: 200,
+  mediaWidth: 400
 };
 
 export const ScenarioExample = Template.bind({});
 ScenarioExample.args = {
-  title: "Expressway_3Lanes",
+  mediaHeight: 200,
+  mediaWidth: 400,
+  height: 613,
+  title: "Expressway_3Lanes ",
   subtitle: "Uploaded 2 hours ago by Jega Sriskantha ",
   labels: [
     {
-      _id: 1,
+      _id: "1",
       color: "#174713",
       description: "National Highways",
       name: "National Highways"
     },
     {
-      _id: 2,
+      _id: "2",
       color: "#1D9586",
       description: "Wet Surface",
       name: "Wet Surface"
     },
     {
-      _id: 3,
+      _id: "3",
       color: "#fcba03",
       description: "Test Label 1",
       name: "Test Label 1"
     },
     {
-      _id: 4,
+      _id: "4",
       color: "#47357a",
       description: "Test Label 2",
       name: "Test Label 2"
     },
     {
-      _id: 5,
+      _id: "5",
       color: "#1D9586",
       description: "Test Label 3",
       name: "Test Label 3"
@@ -87,5 +115,26 @@ ScenarioExample.args = {
         </TableBody>
       </Table>
     </TableContainer>
+  ),
+  width: 436,
+  moreOptionsList: (
+    <List>
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemIcon>
+            <Edit />
+          </ListItemIcon>
+          <ListItemText primary="Edit" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemIcon>
+            <Delete />
+          </ListItemIcon>
+          <ListItemText primary="Delete" />
+        </ListItemButton>
+      </ListItem>
+    </List>
   )
 };
