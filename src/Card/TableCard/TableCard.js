3a4ecdbc2bdec930a756c37ef091f095b5183faf
jegasriskantha <75164177+jegasriskantha@users.noreply.github.com>
2022-11-09 09:26:36 +0000

    Added new table card component.

---
+++ src/Card/TableCard/TableCard.js
@@ -0,0 +1,57 @@
+import {
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography
+} from "@mui/material";
+
+import React from "react";
+
+function TableCard({
+  action = null,
+  tableContent = [],
+  title = "Table",
+  width = 735
+}) {
+  console.log(tableContent);
+  return (
+    <Card sx={{ width }}>
+      <CardHeader
+        title={
+          <Typography
+            sx={{
+              fontSize: 20,
+              fontWeight: 500
+            }}
+            noWrap
+          >
+            {title}
+          </Typography>
+        }
+        action={action}
+      />
+      <CardContent sx={{ padding: 0 }}>
+        <TableContainer component={Box}>
+          <Table size="small">
+            <TableBody>
+              {tableContent.map((row, index) => (
+                <TableRow key={index}>
+                  <TableCell>{row[0]}</TableCell>
+                  <TableCell>{row[1]}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default TableCard;
