df0e9d95e702dd418b59d280d654eec315d56dca
Matthew Corner <matt.corner@ipg-automotive.uk>
2023-01-30 15:16:45 +0000

    Correctly initialise decorator theme state

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -10,7 +10,7 @@ const channel = addons.getChannel();
 addDecorator(story => {
   // this example uses hook but you can also use class component as well
   const [isDark, setDark] = React.useState(
-    () => localStorage.getItem("sb-addon-themes-3" || false) // default to the storybook-dark-mode local storage key
+    () => localStorage.getItem("sb-addon-themes-3").current === "dark" || false // default to the storybook-dark-mode local storage key
   );
 
   React.useEffect(() => {
