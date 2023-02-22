59cbcc307db11aa02c82b37ab35f4947ccde20a3
Manjesh M Pillai <56511816+m4manjesh@users.noreply.github.com>
2023-02-22 13:13:14 +0000

    sidebar updates

--- src/Sidebar/Sidebar.test.js
+++ src/Sidebar/Sidebar.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+
 import React from "react";
 import Sidebar from "./";
 
@@ -8,23 +9,15 @@ describe("Sidebar", () => {
     expect(screen.queryByText("Child")).toBeInTheDocument();
   });
   it("renders app version by default", () => {
-    render(<Sidebar appVersion="v1.2.3" />);
-    expect(screen.queryByText("v1.2.3")).toBeInTheDocument();
+    render(<Sidebar appVersion="1.2.3" />);
+    expect(screen.queryByText("1.2.3")).toBeInTheDocument();
   });
   it("can hide app version", () => {
-    render(<Sidebar appVersion="v1.2.3" showVersion={false} />);
-    expect(screen.queryByText("v1.2.3")).not.toBeInTheDocument();
-  });
-  it("renders icon by default", () => {
-    const { container } = render(<Sidebar logoSrc="/path/img.png" />);
-    expect(
-      container.querySelector("img[src='/path/img.png']")
-    ).toBeInTheDocument();
+    render(<Sidebar appVersion="1.2.3" showVersion={false} />);
+    expect(screen.queryByText("1.2.3")).not.toBeInTheDocument();
   });
   it("can hide icon", () => {
-    const { container } = render(
-      <Sidebar logoSrc="/path/img.png" showLogo={false} />
-    );
+    const { container } = render(<Sidebar showLogo={false} />);
     expect(
       container.querySelector("img[src='/path/img.png']")
     ).not.toBeInTheDocument();
