00cf3661db212e3af27ec61a94397b5d6156997d
Manjesh M Pillai <56511816+m4manjesh@users.noreply.github.com>
2023-02-23 11:44:38 +0000

    adding export

--- src/Sidebar/index.js
+++ src/Sidebar/index.js
@@ -2,3 +2,4 @@ export { default } from "./Sidebar";
 export { default as Sidebar } from "./Sidebar";
 export { default as SidebarDivider } from "./SidebarDivider";
 export { default as SidebarItem } from "./SidebarItem";
+export { default as SidebarLogo } from "./SidebarLogo";
