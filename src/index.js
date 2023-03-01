6d14d845887c03ef9e493f4afeba86693589a248
Manjesh M Pillai <56511816+m4manjesh@users.noreply.github.com>
2023-03-01 08:49:07 +0000

    export app layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export { default as AlignHorizontal } from "./AlignHorizontal";
 export { default as AlignVertical } from "./AlignVertical";
 export { default as AppHeader } from "./AppHeader";
+export { default as AppLayout } from "./AppLayout";
 export { default as Autocomplete } from "./Autocomplete";
 export { default as Canvas } from "./Canvas";
 export { default as CanvasItem } from "./CanvasItem";
