803ab6a8a34ef8811ac274516a1af3791a2756f3
Matthew Corner <matt.corner@ipg-automotive.uk>
2023-04-25 23:27:18 +0100

    Add ThemeProvider tests

--- src/ThemeProvider/ThemeProvider.test.js
+++ src/ThemeProvider/ThemeProvider.test.js
@@ -1,7 +1,32 @@
-import { render, screen } from "@testing-library/react";
+import ThemeProvider, { useTheme } from "./";
+import { act, render, screen } from "@testing-library/react";
+
 import React from "react";
-import { StyledEngineProvider } from "@mui/material/styles";
-import ThemeProvider from "./";
+import { userEvent } from "@storybook/testing-library";
+
+/**
+ * Test component to render the current theme as text
+ */
+function ThemeText() {
+  const [theme] = useTheme();
+  return <p>{theme}</p>;
+}
+
+/**
+ * Test component to toggle the current theme
+ */
+function ThemeToggle() {
+  const [theme, setTheme] = useTheme();
+  const onClick = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+  return (
+    <>
+      <button onClick={onClick}>Toggle</button>
+      <ThemeText />
+    </>
+  );
+}
 
 /**
  * Tests
@@ -9,13 +34,74 @@ import ThemeProvider from "./";
 describe("ThemeProvider", () => {
   test("renders children", () => {
     render(
-      <StyledEngineProvider injectFirst>
-        <ThemeProvider>
-          <div data-testid="child">{"I'm a child"}</div>
-        </ThemeProvider>
-      </StyledEngineProvider>
+      <ThemeProvider>
+        <div data-testid="child">{"I'm a child"}</div>
+      </ThemeProvider>
     );
     const child = screen.getByTestId("child");
     expect(child).toBeInTheDocument();
+  });
+  test("renders light theme by default", () => {
+    // render the component
+    render(
+      <ThemeProvider>
+        <ThemeText />
+      </ThemeProvider>
+    );
+
+    // check that the theme is light
+    const text = screen.getByText("light");
+    expect(text).toBeInTheDocument();
+  });
+  test.each(["light", "dark"])(
+    "renders %s theme when theme controlled",
+    mode => {
+      // render the component with the mode we want to test
+      render(
+        <ThemeProvider theme={mode}>
+          <ThemeText />
+        </ThemeProvider>
+      );
+
+      // check that the theme is the mode we want to test
+      const text = screen.getByText(mode);
+      expect(text).toBeInTheDocument();
+    }
+  );
+  test.each(["light", "dark"])("renders %s theme from local storage", mode => {
+    // set the theme in local storage
+    window.localStorage.setItem("theme", mode);
+
+    // render the component
+    render(
+      <ThemeProvider>
+        <ThemeText />
+      </ThemeProvider>
+    );
+
+    // check that the theme is the mode we want to test
+    const text = screen.getByText(mode);
+    expect(text).toBeInTheDocument();
+  });
+  test.each(["light", "dark"])("renders %s theme when theme toggled", mode => {
+    // first set the theme to the opposite of the mode we want to test
+    window.localStorage.setItem("theme", mode === "light" ? "dark" : "light");
+
+    // render the toggle button
+    render(
+      <ThemeProvider>
+        <ThemeToggle />
+      </ThemeProvider>
+    );
+
+    // click the toggle button
+    const button = screen.getByText("Toggle");
+    act(() => {
+      userEvent.click(button);
+    });
+
+    // check that the theme is now the mode we want to test
+    const text = screen.getByText(mode);
+    expect(text).toBeInTheDocument();
   });
 });
