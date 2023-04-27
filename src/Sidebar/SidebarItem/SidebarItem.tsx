fde4d1fb97144576c4e2a43279d33be9a623d05e
Jega Sriskantha <75164177+jegasriskantha@users.noreply.github.com>
2023-04-27 16:10:31 +0100

    removed extra line

--- src/Sidebar/SidebarItem/SidebarItem.tsx
+++ src/Sidebar/SidebarItem/SidebarItem.tsx
@@ -118,7 +118,6 @@ export default function SidebarItem({
             <CountBadge />
           </ListItemIcon>
           <ExpandIcon />
-
           <ListItemText
             primary={name}
             primaryTypographyProps={primaryTypographyProps}
