bc774c5272ec3fe6872b0c2cb61855e5188f8ee9
Jega Sriskantha <75164177+jegasriskantha@users.noreply.github.com>
2023-05-03 09:38:46 +0100

    Generic App Layout Component

---
+++ src/AppLayout/AppLayout.tsx
@@ -0,0 +1,150 @@
+import { Box, CssBaseline, Drawer, Hidden } from "@mui/material";
+import React, { Fragment, useState } from "react";
+
+import { AppLayoutProps } from "./AppLayout.types";
+import { ConfirmProvider } from "../ConfirmProvider";
+import Sidebar from "../Sidebar";
+import SnackbarProvider from "../SnackbarProvider";
+import ThemeProvider from "../ThemeProvider";
+import AppHeader from "../AppHeader";
+import useTheme from "../ThemeProvider/useTheme";
+
+// app layout component
+function Layout({
+  appVersion,
+  sidebarContent,
+  appName,
+  onChangePassword,
+  onLogout,
+  username,
+  content
+}: AppLayoutProps) {
+  // sidebar styling
+  const sidebarWidth = 100;
+
+  // define state for managing dynamic sidebar
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  // use theme context hook
+  const [theme, setTheme] = useTheme();
+
+  return (
+    <Fragment>
+      <Box height="100vh" display="flex">
+        <CssBaseline />
+        <AppHeader
+          appName={appName}
+          onChangePassword={onChangePassword}
+          onLogout={onLogout}
+          onColourModeChange={newMode => setTheme(newMode)}
+          username={username}
+          mode={theme}
+        />
+        <Box
+          sx={theme => ({
+            [theme.breakpoints.up("md")]: {
+              flexShrink: 0,
+              width: sidebarWidth
+            }
+          })}
+        >
+          <Hidden mdUp>
+            <Drawer
+              variant="temporary"
+              anchor="left"
+              open={mobileOpen}
+              onClose={() => setMobileOpen(false)}
+              sx={{
+                "& .MuiDrawer-paper": {
+                  height: `calc(100% - 64px)`,
+                  paddingTop: "8px",
+                  top: "64px",
+                  width: sidebarWidth
+                }
+              }}
+            >
+              <Sidebar showLogo={false} showVersion={false}>
+                {sidebarContent}
+              </Sidebar>
+            </Drawer>
+          </Hidden>
+          <Hidden mdDown>
+            <Drawer
+              sx={{
+                "& .MuiDrawer-paper": {
+                  height: `calc(100% - 64px)`,
+                  paddingTop: "8px",
+                  top: "64px",
+                  width: sidebarWidth
+                }
+              }}
+              variant="permanent"
+              open
+            >
+              <Sidebar showLogo={false} showVersion={false}>
+                {sidebarContent}
+              </Sidebar>
+            </Drawer>
+          </Hidden>
+        </Box>
+        <Box
+          flexGrow={1}
+          display="flex"
+          flexDirection="column"
+          sx={{
+            background: theme => theme.palette.background.default
+          }}
+        >
+          <Box sx={theme => theme.mixins.toolbar} />
+          <Box
+            sx={theme => ({
+              height: `calc(100vh - ${theme.mixins.toolbar.minHeight}px)`,
+              overflow: "auto",
+              [theme.breakpoints.down("md")]: {
+                width: "100vw"
+              },
+              [theme.breakpoints.up("md")]: {
+                width: `calc(100vw - ${sidebarWidth}px)`
+              }
+            })}
+          >
+            <SnackbarProvider>{content}</SnackbarProvider>
+          </Box>
+        </Box>
+      </Box>
+    </Fragment>
+  );
+}
+
+// app layout wrapper component
+function AppLayout({
+  appVersion,
+  baseUrl,
+  sidebarContent,
+  virtoLogoLinkUrl = "",
+  appName,
+  onChangePassword,
+  onLogout,
+  username,
+  content
+}: AppLayoutProps) {
+  return (
+    <ThemeProvider>
+      <ConfirmProvider>
+        <Layout
+          appVersion={appVersion}
+          baseUrl={baseUrl}
+          sidebarContent={sidebarContent}
+          virtoLogoLinkUrl={virtoLogoLinkUrl}
+          appName={appName}
+          onChangePassword={onChangePassword}
+          onLogout={onLogout}
+          username={username}
+          content={content}
+        />
+      </ConfirmProvider>
+    </ThemeProvider>
+  );
+}
+
+export default AppLayout;
