24b026f5d99084f7775c909b4b305b53b534eb14
nikitharani <nikithanalumachu@gmail.com>
2023-08-23 15:53:25 +0100

    update tests to test selected options

--- src/Autocomplete/Autocomplete.test.tsx
+++ src/Autocomplete/Autocomplete.test.tsx
@@ -34,8 +34,15 @@ describe("Select", () => {
     // click the first option
     await userEvent.click(screen.getByText("option 1"));
 
-    // check that the onChange event is fired
     expect(onChange).toHaveBeenCalledTimes(1);
+
+    // check that the onChange event is fired with the expected value
+    expect(onChange).toHaveBeenCalledWith(
+      expect.anything(),
+      "option 1",
+      "selectOption",
+      { option: "option 1" }
+    );
   });
 
   it("can multi select", async () => {
@@ -60,10 +67,36 @@ describe("Select", () => {
 
     // click the first option
     await userEvent.click(screen.getByText("option 1"));
+
+    expect(onChange).toHaveBeenCalledTimes(1);
+
+    // check that the onChange event is fired with the expected value
+    expect(onChange).toHaveBeenCalledWith(
+      expect.anything(),
+      ["option 1"],
+      "selectOption",
+      { option: "option 1" }
+    );
+
     await userEvent.click(screen.getByRole("button", { name: /open/i }));
+
+    // click the second option
     await userEvent.click(screen.getByText("option 2"));
 
-    // check that the onChange event is fired
     expect(onChange).toHaveBeenCalledTimes(2);
+
+    // check that the onChange event is fired with the expected value
+    expect(onChange).toHaveBeenCalledWith(
+      expect.anything(),
+      ["option 2"],
+      "selectOption",
+      { option: "option 2" }
+    );
+
+    // Check the first call with "option 1"
+    expect(onChange.mock.calls[0][1]).toEqual(["option 1"]);
+
+    // Check the second call with "option 2"
+    expect(onChange.mock.calls[1][1]).toEqual(["option 2"]);
   });
 });
