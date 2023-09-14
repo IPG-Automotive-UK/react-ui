de90ca2556968d252535b824405d9f658fb65d53
Victor Lucas <106325764+PickleVL@users.noreply.github.com>
2023-09-14 10:31:59 +0100

    Fixing syntax errors on playwright test and removing playwright action

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Match test files using this regular expression */
-  testMatch: /.*(spec)\.(js|ts|mjs|tsx)/,
+  testMatch: /.*(spec)\.(js|ts|mjs)/,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
