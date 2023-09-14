0e8855d3827172bbe6911dd546d2d3b87bbd8267
Matthew Corner <matt.corner@ipg-automotive.uk>
2023-09-14 09:55:02 +0100

    Add story that mimics the VIRTO.RESULT use case

--- src/LinePlot/LinePlot.stories.tsx
+++ src/LinePlot/LinePlot.stories.tsx
@@ -1,5 +1,7 @@
+import { Box, IconButton, Typography } from "@mui/material";
 import { Meta, StoryFn } from "@storybook/react";
 
+import { DragIndicatorOutlined } from "@mui/icons-material";
 import LinePlot from "./LinePlot";
 import { LinePlotProps } from "./LinePlot.types";
 import React from "react";
@@ -27,6 +29,41 @@ const Template: StoryFn<LinePlotProps> = args => {
   return <LinePlot {...args} />;
 };
 
+const TemplateWithCard: StoryFn<LinePlotProps> = args => {
+  return (
+    <Box
+      sx={{
+        alignItems: "flex-start",
+        border:
+          "0.5px solid var(--light-other-outlined-border-23-p, rgba(0, 0, 0, 0.23))",
+        borderRadius: "6px",
+        boxShadow: "0px 0px 0px 1px #E0E0E0",
+        display: "flex",
+        flex: "1 0 0",
+        flexDirection: "column",
+        gap: "24px",
+        height: "332px",
+        maxWidth: "900px",
+        minWidth: "500px"
+      }}
+    >
+      <Box
+        sx={{ alignItems: "center", display: "flex", padding: "24px", pb: 0 }}
+      >
+        <IconButton>
+          <DragIndicatorOutlined color="primary" />
+        </IconButton>
+        <Typography sx={{ fontSize: "20px", fontWeight: 500 }} color="primary">
+          Testing with Manjesh
+        </Typography>
+      </Box>
+      <Box sx={{ height: "100%", width: "100%" }}>
+        <LinePlot {...args} />
+      </Box>
+    </Box>
+  );
+};
+
 // Default
 export const Default = {
   args: {
@@ -39,4 +76,18 @@ export const Default = {
     ylabel: "Y"
   },
   render: Template
+};
+
+// Card
+export const Card = {
+  args: {
+    markers: false,
+    showTitle: false,
+    title: "",
+    xdata: [0, 1, 2, 3, 4, 5],
+    xlabel: "X",
+    ydata: [0, 20, 30, 40, 50],
+    ylabel: "Y"
+  },
+  render: TemplateWithCard
 };
