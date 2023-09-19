130c51dc119aec8e885c6ac35502815b29d9f03e
David Barry <david.barry@ipg-automotive.com>
2023-09-19 09:36:12 +0100

    type def for useSnackbar

--- src/SnackbarProvider/index.d.ts
+++ src/SnackbarProvider/index.d.ts
@@ -1,7 +1,11 @@
+import { Context } from "react";
+
 export type SnackbarProviderProps = {
   children: React.ReactNode;
 };
 
 declare const SnackbarProvider: React.FC<SnackbarProviderProps>;
 
-export { SnackbarProvider };
+declare function useSnackbar<T>(context: Context<T> | null): T;
+
+export { SnackbarProvider, useSnackbar };
