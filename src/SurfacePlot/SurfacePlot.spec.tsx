24d4572c7a1b51ad616c76fd20e8b1685287b1e7
syedsalehinipg <syed.salehin@ipg-automotive.uk>
2023-09-22 15:27:06 +0100

    debug try 2

--- src/SurfacePlot/SurfacePlot.spec.tsx
+++ src/SurfacePlot/SurfacePlot.spec.tsx
@@ -1,28 +1,31 @@
 import { expect, test } from "@playwright/test";
 
-// This test checks that the SurfacePlot component can be displayed in fullscreen mode
-// and that the title of the fullscreen dialog is visible.
-
 test("Surface plot in fullscreen and title visible", async ({ page }) => {
-  // Navigate to the SurfacePlot component in Storybook
   await page.goto(
-    "http://localhost:6006/?path=/story/plots-surfaceplot--default"
+    "http://localhost:6006/?path=/story/plots-surfaceplot--default",
+    { waitUntil: "networkidle" }
   );
 
-  // Click the fullscreen button in the Plotly toolbar
+  await page.waitForTimeout(2000);
+  await page.waitForSelector('iframe[title="storybook-preview-iframe"]');
+
+  await expect(
+    page
+      .frameLocator('iframe[title="storybook-preview-iframe"]')
+      .locator("div:nth-child(4) > .modebar-btn")
+  ).toBeVisible();
+
   await page
     .frameLocator('iframe[title="storybook-preview-iframe"]')
     .locator("div:nth-child(4) > .modebar-btn")
     .click();
 
-  // Assert that the fullscreen dialog is visible
   await expect(
     page
       .frameLocator('iframe[title="storybook-preview-iframe"]')
       .locator(".MuiDialogContent-root")
   ).toBeVisible();
 
-  // Assert that the title of the fullscreen dialog is visible
   await expect(
     page
       .frameLocator('iframe[title="storybook-preview-iframe"]')
