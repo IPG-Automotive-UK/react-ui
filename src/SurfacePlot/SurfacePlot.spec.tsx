092ca987cd77ddd7fc9079cfc514b782b54131cb
syedsalehinipg <syed.salehin@ipg-automotive.uk>
2023-10-02 11:05:40 +0100

    debug try 1 revert

--- src/SurfacePlot/SurfacePlot.spec.tsx
+++ src/SurfacePlot/SurfacePlot.spec.tsx
@@ -6,31 +6,18 @@ test("Surface plot in fullscreen and title visible", async ({ page }) => {
     "http://localhost:6006/?path=/story/plots-surfaceplot--default"
   );
 
+  // Click the fullscreen button in the Plotly toolbar
   await page
     .frameLocator('iframe[title="storybook-preview-iframe"]')
     .locator("div:nth-child(4) > .modebar-btn")
     .click();
-  await page.getByLabel("Surface Plot", { exact: true }).fill("Test");
 
-  await expect(
-    page
-      .frameLocator('iframe[title="storybook-preview-iframe"]')
-      .locator(".MuiDialogContent-root")
-  ).toBeVisible();
-  await expect(page.getByText("Test")).toBeVisible();
+  // Get the title of the fullscreen dialog
+  const title = await page
+    .frameLocator('iframe[title="storybook-preview-iframe"]')
+    .locator(".MuiDialogTitle-root")
+    .textContent();
 
-  // // Click the fullscreen button in the Plotly toolbar
-  // await page
-  //   .frameLocator('iframe[title="storybook-preview-iframe"]')
-  //   .locator("div:nth-child(4) > .modebar-btn")
-  //   .click();
-
-  // // Get the title of the fullscreen dialog
-  // const title = await page
-  //   .frameLocator('iframe[title="storybook-preview-iframe"]')
-  //   .locator(".MuiDialogTitle-root")
-  //   .textContent();
-
-  // // Assert that the title is equal to "Surface Plot"
-  // expect(title).toBe("Surface Plot");
+  // Assert that the title is equal to "Surface Plot"
+  expect(title).toBe("Surface Plot");
 });
