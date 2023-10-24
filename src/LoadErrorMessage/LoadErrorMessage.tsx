a94db1dad0b90131a57672711778d944b280c1f9
syedsalehinipg <syed.salehin@ipg-automotive.uk>
2023-10-24 16:59:45 +0100

    dark mode and improvements

--- src/LoadErrorMessage/LoadErrorMessage.tsx
+++ src/LoadErrorMessage/LoadErrorMessage.tsx
@@ -1,8 +1,8 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Alert, Button, Paper, Typography } from "@mui/material";
 import React, { useState } from "react";
 
 import { LoadErrorMessageProps } from "./LoadErrorMessage.types";
-import { ReactComponent as VirtoThinking } from "../../static/VirtoThinking.svg";
+import VirtoThinking from "../SvgIcons/VirtoMascots/VirtoThinking";
 
 // LoadErrorMessage component
 const LoadErrorMessage = ({
@@ -30,23 +30,28 @@ const LoadErrorMessage = ({
 
   // Render the LoadErrorMessage component
   return (
-    <Box
-      display="flex"
-      width="320px"
-      padding="24px"
-      flexDirection="column"
-      alignItems="center"
-      gap="16px"
-      borderRadius="3px"
-      border={
-        "1px solid var(--light-other-outlined-border-23-p, rgba(0, 0, 0, 0.23))"
-      }
-      style={{
-        background: "#FFF"
-      }}
+    <Paper
+      sx={theme => ({
+        alignItems: "center",
+        border:
+          theme.palette.mode === "dark"
+            ? "1px solid var(--dark-other-outlined-border-23-p, rgba(255, 255, 255, 0.23))"
+            : "1px solid var(--light-other-outlined-border-23-p, rgba(0, 0, 0, 0.23))",
+        borderRadius: "3px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "16px",
+        padding: "24px",
+        width: "320px"
+      })}
     >
       {/* Render the VirtoThinking SVG if showImg prop is true */}
-      {showImg && <VirtoThinking data-testid="virto-thinking-svg" />}
+      {showImg && (
+        <VirtoThinking
+          data-testid="virto-thinking-svg"
+          sx={{ height: 126, width: 150 }}
+        />
+      )}
       {/* Render the error title */}
       <Typography
         variant="h6"
@@ -60,7 +65,11 @@ const LoadErrorMessage = ({
       <Typography
         variant="body2"
         textAlign="center"
-        color="rgba(0, 0, 0, 0.87)"
+        color={theme =>
+          theme.palette.mode === "dark"
+            ? "var(--dark-text-secondary, rgba(255, 255, 255, 0.70))"
+            : "rgba(0, 0, 0, 0.87)"
+        }
       >
         <span>{message}</span>
       </Typography>
@@ -76,53 +85,18 @@ const LoadErrorMessage = ({
           {/* Render the "Hide More Details" text */}
           <Typography
             variant="body2"
-            color="var(--light-primary-main, #003063)"
+            color={theme =>
+              theme.palette.mode === "dark"
+                ? "var(--dark-primary-main, #87A5D2)"
+                : "var(--light-primary-main, #003063)"
+            }
             style={{ cursor: "pointer" }}
             onClick={handleDetailsClick}
           >
             Hide More Details
           </Typography>
           {/* Render the error details */}
-          <Box
-            display="flex"
-            gap="10px"
-            width="320px"
-            alignItems="center"
-            flexDirection="column"
-            borderRadius="4px"
-            style={{
-              background:
-                "var(--light-error-shades-190-p, linear-gradient(0deg, rgba(255, 255, 255, 0.90) 0%, rgba(255, 255, 255, 0.90) 100%), #D32F2F)"
-            }}
-          >
-            <Box
-              display="flex"
-              padding="8px"
-              flexDirection="column"
-              alignItems="center"
-              gap="4px"
-              flex="1 0 0"
-            >
-              {/* Render the error icon and details */}
-              <Box display="flex" alignItems="center" gap="12px">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="#D32F2F"
-                >
-                  <path d="M11 15H13V17H11V15ZM11 7H13V13H11V7ZM11.99 2C6.47 2 2 6.48 2 12C2 17.52 6.47 22 11.99 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 11.99 2ZM12 20C7.58 20 4 16.42 4 12C4 7.58 7.58 4 12 4C16.42 4 20 7.58 20 12C20 16.42 16.42 20 12 20Z" />
-                </svg>
-                <Typography
-                  variant="body2"
-                  color="var(--light-error-shades-160-p, #D32F2F)"
-                >
-                  {errorDetails}
-                </Typography>
-              </Box>
-            </Box>
-          </Box>
+          <Alert severity="error">{errorDetails}</Alert>
         </>
       )}
 
@@ -130,7 +104,11 @@ const LoadErrorMessage = ({
       {errorDetails && !detailsVisible && (
         <Typography
           variant="body2"
-          color="var(--light-primary-main, #003063)"
+          color={theme =>
+            theme.palette.mode === "dark"
+              ? "var(--dark-primary-main, #87A5D2)"
+              : "var(--light-primary-main, #003063)"
+          }
           style={{ cursor: "pointer" }}
           onClick={handleDetailsClick}
         >
@@ -139,23 +117,29 @@ const LoadErrorMessage = ({
       )}
       {/* Render the contact team text */}
       <Typography
-        fontFamily="Montserrat"
         fontSize="12px"
         lineHeight="20x"
         letterSpacing="0.17px"
         fontWeight="400"
-        color="var(--light-text-primary, rgba(0, 0, 0, 0.87))"
+        color={theme =>
+          theme.palette.mode === "dark"
+            ? "var(--dark-text-secondary, rgba(255, 255, 255, 0.70))"
+            : "rgba(0, 0, 0, 0.87)"
+        }
       >
         If this persists, contact {/* Render the contact team link */}
         {supportUrl ? (
           <Typography
-            fontFamily="Montserrat"
             fontSize="12px"
             fontStyle="normal"
             fontWeight="400"
             lineHeight="166%"
             letterSpacing="0.4px"
-            color="var(--light-primary-main, #003063)"
+            color={theme =>
+              theme.palette.mode === "dark"
+                ? "var(--dark-primary-main, #87A5D2)"
+                : "var(--light-primary-main, #003063)"
+            }
             display="inline"
             style={{ cursor: "pointer", textDecoration: "underline" }}
             onClick={handleSupportClick}
@@ -164,7 +148,6 @@ const LoadErrorMessage = ({
           </Typography>
         ) : (
           <Typography
-            fontFamily="Montserrat"
             fontSize="12px"
             fontStyle="normal"
             fontWeight="400"
@@ -176,7 +159,7 @@ const LoadErrorMessage = ({
           </Typography>
         )}
       </Typography>
-    </Box>
+    </Paper>
   );
 };
 
--- src/LoadErrorMessage/LoadErrorMessage.types.ts
+++ src/LoadErrorMessage/LoadErrorMessage.types.ts
@@ -1,10 +1,34 @@
 export type LoadErrorMessageProps = {
-  showImg: boolean; // Whether to show the VirtoThinking SVG or not
-  title: string; // The title of the error message
-  message: string; // The main message of the error message
-  errorDetails?: string; // The details of the error message (optional)
-  contactTeam: "Support" | "Customer Service"; // The team to contact if the error persists
-  supportUrl?: string; // The URL to the support page (optional)
-  actionButtonText?: string; // The text to display on the action button (optional)
-  onButtonClick?: () => void; // The function to call when the action button is clicked (optional)
+  /**
+   * Whether to show the VirtoThinking SVG or not.
+   */
+  showImg: boolean;
+  /**
+   * The title of the error message.
+   */
+  title: string;
+  /**
+   * The main message of the error message.
+   */
+  message: string;
+  /**
+   * The details of the error message (optional).
+   */
+  errorDetails?: string;
+  /**
+   * The team to contact if the error persists.
+   */
+  contactTeam: "Support" | "Customer Service";
+  /**
+   * The URL to the support page (optional).
+   */
+  supportUrl?: string;
+  /**
+   * The text to display on the action button (optional).
+   */
+  actionButtonText?: string;
+  /**
+   * The function to call when the action button is clicked (optional).
+   */
+  onButtonClick?: () => void;
 };
