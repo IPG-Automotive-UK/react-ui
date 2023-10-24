80c9058308df49202d4e935d8dd965e329d73145
syedsalehinipg <syed.salehin@ipg-automotive.uk>
2023-10-24 16:59:14 +0100

    added SvgIcon component added

---
+++ src/SvgIcons/VirtoMascots/VirtoThinking/VirtoThinking.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryFn } from "@storybook/react";
+
+import React from "react";
+import VirtoThinking from "./VirtoThinking";
+import { VirtoThinkingProps } from "./VirtoThinking.types";
+
+/**
+ * Story metadata
+ */
+const meta: Meta<typeof VirtoThinking> = {
+  component: VirtoThinking,
+  title: "General/SvgIcons/VirtoMascots/VirtoThinking"
+};
+export default meta;
+
+const Template: StoryFn<VirtoThinkingProps> = args => {
+  return <VirtoThinking {...args} />;
+};
+
+export const Default = {
+  args: {
+    sx: { height: 126, width: 150 }
+  },
+
+  render: Template
+};
