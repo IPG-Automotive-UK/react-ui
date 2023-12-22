7a0d4f738603feb299f9c79aac8dcb9212226818
Matthew Corner <matt.corner@ipg-automotive.uk>
2023-12-22 13:20:15 +0000

    Document fcn

--- src/utils/readAsDataURL.ts
+++ src/utils/readAsDataURL.ts
@@ -1,3 +1,13 @@
+/**
+ * Reads a file as a data URL
+ * @param file File to read
+ * @returns Promise that resolves to a data URL
+ *
+ * @example
+ * const file = new File([...], "filename.png", { type: "image/png" });
+ * const dataURL = await readAsDataURL(file);
+ * // dataURL = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAA..."
+ */
 export function readAsDataURL(file: File): Promise<string> {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
