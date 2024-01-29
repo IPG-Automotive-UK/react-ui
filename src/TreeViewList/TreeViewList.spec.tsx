a2ef12c211a074bcbe7d1c2006f28a32a8dcddd0
syedsalehinipg <syed.salehin@ipg-automotive.uk>
2024-01-29 18:29:15 +0000

    search data and tests

--- src/TreeViewList/TreeViewList.spec.tsx
+++ src/TreeViewList/TreeViewList.spec.tsx
@@ -85,28 +85,48 @@ test("should hide tooltip when not hovering over parameter", async ({
   await page.goto(
     "http://localhost:6006/?path=/story/lists-treeviewlist--default"
   );
-  await page
-    .frameLocator('iframe[title="storybook-preview-iframe"]')
-    .getByText("TRM")
-    .click();
-  await page
-    .frameLocator('iframe[title="storybook-preview-iframe"]')
-    .getByText("GearSpred")
-    .hover();
-  await expect(
-    page
-      .frameLocator('iframe[title="storybook-preview-iframe"]')
-      .getByText("TRM.GearSpredEfficieny for")
-  ).toBeVisible();
 
-  // Move the mouse to the top left corner of the page to simulate "unhovering"
-  await page.mouse.move(0, 0);
+  // Wait for the iframe to be attached in the DOM.
+  await page.waitForSelector('iframe[title="storybook-preview-iframe"]');
 
-  await expect(
-    page
-      .frameLocator('iframe[title="storybook-preview-iframe"]')
-      .getByText("TRM.GearSpred Efficieny for")
-  ).toBeHidden();
+  // Get the Frame object for the iframe.
+  const frameElement = await page.$('iframe[title="storybook-preview-iframe"]');
+
+  if (frameElement !== null) {
+    const frame = await frameElement.contentFrame();
+
+    if (frame !== null) {
+      // Wait for the '#storybook-root ul li' to be attached in the DOM.
+      await frame.waitForSelector("#storybook-root ul li");
+
+      await page
+        .frameLocator('iframe[title="storybook-preview-iframe"]')
+        .getByText("AER")
+        .click();
+      await page
+        .frameLocator('iframe[title="storybook-preview-iframe"]')
+        .getByText("FrontalArea")
+        .hover();
+      await expect(
+        page
+          .frameLocator('iframe[title="storybook-preview-iframe"]')
+          .getByText("AER.FrontalArea")
+      ).toBeVisible();
+
+      // Move the mouse to the top left corner of the page to simulate "unhovering"
+      await page.mouse.move(0, 0);
+
+      await expect(
+        page
+          .frameLocator('iframe[title="storybook-preview-iframe"]')
+          .getByText("TRM.GearSpred Efficieny for")
+      ).toBeHidden();
+    } else {
+      throw new Error("Frame object is null");
+    }
+  } else {
+    throw new Error("Frame element is null");
+  }
 });
 
 /**
@@ -116,47 +136,68 @@ test("should hide child when is collapsed", async ({ page }) => {
   await page.goto(
     "http://localhost:6006/?path=/story/lists-treeviewlist--default"
   );
-  await page
-    .frameLocator('iframe[title="storybook-preview-iframe"]')
-    .getByText("TRM")
-    .click();
-  await expect(
-    page
-      .frameLocator('iframe[title="storybook-preview-iframe"]')
-      .getByText("GearSpred", { exact: true })
-  ).toBeVisible();
-  await page
-    .frameLocator('iframe[title="storybook-preview-iframe"]')
-    .getByText("TRM")
-    .click();
-  await expect(
-    page
-      .frameLocator('iframe[title="storybook-preview-iframe"]')
-      .getByText("GearSpred", { exact: true })
-  ).toBeHidden();
+
+  const frameElement = await page.$('iframe[title="storybook-preview-iframe"]');
+
+  if (frameElement !== null) {
+    const frame = await frameElement.contentFrame();
+
+    if (frame !== null) {
+      await page
+        .frameLocator('iframe[title="storybook-preview-iframe"]')
+        .getByText("TRM")
+        .click();
+      await expect(
+        page
+          .frameLocator('iframe[title="storybook-preview-iframe"]')
+          .getByText("Efficiency1D", { exact: true })
+      ).toBeVisible();
+      await page
+        .frameLocator('iframe[title="storybook-preview-iframe"]')
+        .getByText("TRM")
+        .click();
+      await expect(
+        page
+          .frameLocator('iframe[title="storybook-preview-iframe"]')
+          .getByText("Efficiency1D", { exact: true })
+      ).toBeHidden();
+    } else {
+      throw new Error("Frame object is null");
+    }
+  } else throw new Error("Frame element is null");
 });
 
 test("should expand the parent and child nodes that match the search term", async ({
   page
 }) => {
   await page.goto(
-    "http://localhost:6006/?path=/story/lists-treeviewlist--default&args=expandSearchTerm:true;searchTerm:ratio"
+    "http://localhost:6006/?path=/story/lists-treeviewlist--default&args=expandSearchTerm:true;searchTerm:brk+pedal+ratio"
   );
-  await expect(
-    page
-      .frameLocator('iframe[title="storybook-preview-iframe"]')
-      .getByText("BRK")
-  ).toBeVisible();
-  await expect(
-    page
-      .frameLocator('iframe[title="storybook-preview-iframe"]')
-      .getByText("Pedal")
-  ).toBeVisible();
-  await expect(
-    page
-      .frameLocator('iframe[title="storybook-preview-iframe"]')
-      .getByText("Ratio", { exact: true })
-  ).toBeVisible();
+
+  // Wait for the iframe to be attached in the DOM.
+  await page.waitForSelector('iframe[title="storybook-preview-iframe"]');
+
+  // Get the Frame object for the iframe.
+  const frameElement = await page.$('iframe[title="storybook-preview-iframe"]');
+
+  if (frameElement !== null) {
+    const frame = await frameElement.contentFrame();
+
+    if (frame !== null) {
+      // Wait for the '#storybook-root ul li' to be attached in the DOM.
+      await frame.waitForSelector("#storybook-root ul li");
+
+      const liElements = await frame.$$("#storybook-root ul li");
+
+      const elementCount = liElements.length;
+
+      expect(elementCount).toBe(1);
+    } else {
+      throw new Error("Frame object is null");
+    }
+  } else {
+    throw new Error("Frame element is null");
+  }
 });
 
 test("should not expand the child nodes when expand for search is not enabled", async ({
--- src/TreeViewList/TreeViewList.stories.tsx
+++ src/TreeViewList/TreeViewList.stories.tsx
@@ -3,6 +3,7 @@ import { Item, TreeViewListProps } from "./TreeViewList.types";
 import { Meta } from "@storybook/react";
 import React from "react";
 import TreeViewList from "./TreeViewList";
+import data from "./data.json";
 
 /**
  * Story metadata
@@ -21,86 +22,7 @@ const Template = <T,>(args: TreeViewListProps<Item<T>>) => {
 // Default
 export const Default = {
   args: {
-    items: [
-      {
-        children: [
-          {
-            children: [
-              {
-                children: [
-                  {
-                    name: "PumpMaxDelivery",
-                    options: [],
-                    tooltip:
-                      "Maximum delivery of the two hydraulic pumps (each responsible for one circuit) at 0 bar pressure difference. Parameter needed for CarMaker hydraulic ESC (Name: 'Pump.qMax')."
-                  }
-                ],
-                name: "HydESPModel"
-              },
-              {
-                name: "BoreTravel",
-                options: [],
-                tooltip:
-                  "Piston travel to close compensation bore inside master cylinder. Parameter needed for CarMaker hydraulic ESC (Name: 'MC.xCompBore')."
-              }
-            ],
-            name: "MCbooster"
-          },
-          {
-            children: [
-              {
-                name: "Ratio",
-                options: [],
-                tooltip:
-                  "The pedal ratio amplifies the force of the brake pedal. Parameter needed for CarMaker hydraulic ESC (Name: 'Pedal.ratio')."
-              },
-              {
-                name: "ResponseTime",
-                options: [],
-                tooltip:
-                  "Period of time from pressing the pedal till the brake pressure begins to build up."
-              }
-            ],
-            name: "Pedal"
-          }
-        ],
-        id: "64f1b6fd511d08b5f6bc2a1e",
-        name: "BRK"
-      },
-      {
-        children: [
-          {
-            children: [
-              {
-                children: [
-                  {
-                    name: "Capacity",
-                    options: [],
-                    tooltip:
-                      "This is the total capacity of the board high voltage battery"
-                  }
-                ],
-                name: "HV"
-              }
-            ],
-            name: "Battery"
-          }
-        ],
-        name: "ELE"
-      },
-      {
-        children: [
-          { name: "Efficiency1D", options: [] },
-          {
-            name: "GearSpred",
-            options: [],
-            tooltip:
-              "Efficieny for all gear numbers. If the number of efficiencies are less than the number of gears, the last entry in the table will be applied to all remaining gears."
-          }
-        ],
-        name: "TRM"
-      }
-    ]
+    items: data
   },
   render: Template
 };
