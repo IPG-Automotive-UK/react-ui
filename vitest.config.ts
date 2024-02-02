27eefe1bbed33cdb55590f7fe70e5ee6a3c20638
Matthew Corner <matt.corner@ipg-automotive.uk>
2024-02-02 21:03:50 +0000

    Sort code coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,17 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    coverage: {
+      exclude: [
+        "coverage/**",
+        "dist/**",
+        "**/[.]**",
+        "**/*.d.ts",
+        "**/*{.,-}{test,spec,stories,types}.?(c|m)[jt]s?(x)",
+        "**/index.[jt]s?(x)"
+      ],
+      include: ["src/**/*"]
+    },
     environment: "jsdom",
     globals: true,
     include: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(test).[jt]s?(x)"],
