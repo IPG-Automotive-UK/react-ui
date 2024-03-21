6053f8c0ec280c34cffc371590224d0d457179d2
Luke Jones <luke@Lukes-MacBook-Pro.local>
2024-03-21 14:20:41 +0000

    Add story with remix validated form wrapper and basic zod schema

--- src/ValidatedForm/ValidatedInput/ValidatedInput.stories.tsx
+++ src/ValidatedForm/ValidatedInput/ValidatedInput.stories.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { StoryFn } from "@storybook/react";
+import { ValidatedForm } from "remix-validated-form";
 import ValidatedInput from "./";
 import { ValidatedInputProps } from "./ValidatedInput.types";
+import { withZod } from "@remix-validated-form/with-zod";
+import { z } from "zod";
 
 export default {
   component: ValidatedInput,
   title: "ValidatedForm/ValidatedInput"
 };
 
+const validator = withZod(
+  z.object({
+    name: z.string().min(1, { message: "Name is required" })
+  })
+);
+
 const Template: StoryFn<ValidatedInputProps> = args => {
-  return <ValidatedInput {...args} />;
+  return (
+    <ValidatedForm validator={validator}>
+      <ValidatedInput {...args} />;
+    </ValidatedForm>
+  );
 };
 
 export const Default = {
