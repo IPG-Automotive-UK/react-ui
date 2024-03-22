80567c046545b7e9d4a030f17bab207942ad9037
Joseph Meyrick <126706377+Skoob1905@users.noreply.github.com>
2024-03-22 10:27:13 +0000

    Create a Selector Button (#834)
    * SelectorButton added
    
    * test fixes
    
    * removed repeated type object
    
    * added to src/index.ts
    
    * linting

---
+++ src/SelectorButton/SelectorButton.stories.tsx
@@ -0,0 +1,25 @@
+import AddHomeIcon from "@mui/icons-material/AddHome";
+import React from "react";
+import SelectorButton from "./SelectorButton";
+import { SelectorButtonProps } from ".";
+import { StoryFn } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+export default { component: SelectorButton, title: "Selectors/SelectorButton" };
+
+const Template: StoryFn<SelectorButtonProps> = args => {
+  return <SelectorButton {...args} />;
+};
+
+export const Default: {
+  args: SelectorButtonProps;
+  render: StoryFn<SelectorButtonProps>;
+} = {
+  args: {
+    description: "Let's add a new house",
+    icon: <AddHomeIcon sx={{ fontSize: 56 }} />,
+    onClick: action("onClick"),
+    text: "Add House"
+  },
+  render: Template
+};
---
+++ src/SelectorButton/SelectorButton.tsx
@@ -0,0 +1,48 @@
+import { ButtonBase, Typography } from "@mui/material";
+
+import React from "react";
+import { SelectorButtonProps } from ".";
+
+// Selector Buttons for scene/upload page
+export default function SelectorButton({
+  icon,
+  text,
+  description,
+  onClick
+}: SelectorButtonProps) {
+  return (
+    <ButtonBase
+      onClick={onClick}
+      sx={{
+        "&:hover": {
+          boxShadow: "0 1px 5px 1px rgba(0,0,0,.23)"
+        },
+        alignItems: "center",
+        background: theme => theme.palette.background.paper,
+        border: theme => `1px solid ${theme.palette.divider}`,
+        borderRadius: 4,
+        display: "flex",
+        flexDirection: "column",
+        height: 248,
+        p: 2,
+        textAlign: "left",
+        transition: ".2s ease-in-out",
+        width: 336
+      }}
+    >
+      {icon}
+      <Typography
+        variant="h6"
+        sx={{
+          fontWeight: 600,
+          lineHeight: 1,
+          mb: 1,
+          mt: 2
+        }}
+      >
+        {text}
+      </Typography>
+      <Typography variant="body2">{description}</Typography>
+    </ButtonBase>
+  );
+}
