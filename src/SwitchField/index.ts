decdacebe1a7f9688f7899ac08fe0d538c9d739f
Luke Jones <luke@Lukes-MacBook-Pro.local>
2024-03-25 07:50:38 +0000

    Add comments to switch prop types

--- src/SwitchField/index.ts
+++ src/SwitchField/index.ts
@@ -1,15 +1,45 @@
 import SwitchField from "./SwitchField";
 import type { SwitchProps } from "@mui/material";
 
+/**
+ * Prop type for switch fields
+ */
 export type SwitchFieldProps = {
+  /**
+   * Is the switch checked by the user
+   */
   checked?: boolean;
+  /**
+   * Is the switch checked by default
+   */
   defaultChecked?: boolean;
+  /**
+   * Is the switch disabled
+   */
   disabled?: boolean;
+  /**
+   * The helper text for the switch
+   */
   helperText?: string;
+  /**
+   * The label for the switch
+   */
   label?: string;
+  /**
+   * The name of the switch
+   */
   name?: string;
+  /**
+   * Function to call when the switch changes
+   */
   onChange: SwitchProps["onChange"];
+  /**
+   * The two toggle options for the switch
+   */
   options?: [string, string];
+  /**
+   * The size of the switch
+   */
   size?: "small" | "medium";
 };
 
