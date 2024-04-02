cfe59b336c654244313f3b14d49a4e27ad78dacf
syedsalehinipg <syed.salehin@ipg-automotive.uk>
2024-04-02 10:46:06 +0100

    created VehicleSelector component

---
+++ src/VehicleSelector/VehicleSelector.stories.tsx
@@ -0,0 +1,73 @@
+import { Meta, StoryFn } from "@storybook/react";
+
+import React from "react";
+import { SelectedVehicle } from "../VehicleSelect/VehicleSelect.types";
+import VehicleSelector from "./VehicleSelector";
+import { VehicleSelectorProps } from "./VehicleSelector.types";
+import { action } from "@storybook/addon-actions";
+import { useArgs } from "@storybook/preview-api";
+
+/**
+ * Story metadata
+ */
+const meta: Meta<typeof VehicleSelector> = {
+  component: VehicleSelector,
+  title: "Selectors/VehicleSelector"
+};
+
+export default meta;
+// Default story with all props
+const Template: StoryFn<VehicleSelectorProps> = args => {
+  // useArgs is a hook that returns the current state of the args object
+  const [{ value }, updateArgs] = useArgs<VehicleSelectorProps>();
+
+  // update the args object with the new value value
+  React.useEffect(() => {
+    updateArgs({ value });
+  }, [value, updateArgs]);
+
+  // callback for when the selected vehicles change
+  const onChange = (selectedVehicle: SelectedVehicle[]) => {
+    updateArgs({ value: selectedVehicle });
+    action("onChange")(selectedVehicle);
+  };
+
+  return <VehicleSelector {...args} onChange={onChange} value={value} />;
+};
+
+export const Default = {
+  args: {
+    flexDirection: "column",
+    flexWrap: "nowrap",
+    gates: [],
+    value: [],
+    variants: [
+      {
+        _id: "64c8c4cccc8d6f00130b366b",
+        modelYear: "2015",
+        projectCode: "911",
+        variant: "MP - 3.6 l6 - 397kW - 7MT - R20"
+      },
+      {
+        _id: "64c8c4cccc8d6f00130b367e",
+        modelYear: "2015",
+        projectCode: "911",
+        variant: "JS - 3.6 l6 - 397kW - 7MT - R20"
+      },
+      {
+        _id: "64c8c4cccc8d6f00130b3691",
+        modelYear: "2016",
+        projectCode: "911",
+        variant: "DB - 3.6 l6 - 397kW - 7MT - R20"
+      },
+      {
+        _id: "64c8c4cccc8d6f00130b36a4",
+        modelYear: "2016",
+        projectCode: "911",
+        variant: "MC - 397kW - 7MT - R20"
+      }
+    ]
+  },
+
+  render: Template
+};
