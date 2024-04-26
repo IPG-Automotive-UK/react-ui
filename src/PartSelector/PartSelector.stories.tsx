a57b9580082b061d090443fc95877c3a5307efd8
syedsalehinipg <syed.salehin@ipg-automotive.uk>
2024-04-26 14:54:14 +0100

    Added PartSelector component, stories and its related files

---
+++ src/PartSelector/PartSelector.stories.tsx
@@ -0,0 +1,123 @@
+import { Meta, StoryFn } from "@storybook/react";
+
+import { Part } from "../PartSelector/PartSelector.types";
+import PartSelector from "./PartSelector";
+import { PartSelectorProps } from "./PartSelector.types";
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import { useArgs } from "@storybook/preview-api";
+
+/**
+ * Story metadata
+ */
+const meta: Meta<typeof PartSelector> = {
+  component: PartSelector,
+  title: "Selectors/PartSelector"
+};
+
+export default meta;
+// Default story with all props
+const Template: StoryFn<PartSelectorProps> = args => {
+  // useArgs is a hook that returns the current state of the args object
+  const [{ value }, updateArgs] = useArgs<PartSelectorProps>();
+
+  // update the args object with the new value value
+  React.useEffect(() => {
+    updateArgs({ value });
+  }, [value, updateArgs]);
+
+  // callback for when the selected part change
+  const onChange = (selectedPart: Part) => {
+    updateArgs({ value: selectedPart });
+    action("onChange")(selectedPart);
+  };
+
+  return <PartSelector {...args} onChange={onChange} value={value} />;
+};
+
+// Dataset of parts
+const parts = [
+  {
+    _id: "634957b1bcd57200132d45df",
+    part_name: "Low Speed Motor 1 Gear",
+    part_number: "5869274521"
+  },
+  {
+    _id: "634957b1bcd57200132d45dg",
+    part_name: "Low Speed Motor 1 Gear",
+    part_number: "864564655"
+  },
+  {
+    _id: "634957b1bcd57200132d45dh",
+    part_name: "Low Speed Motor 1 Gear",
+    part_number: "456846454"
+  },
+  {
+    _id: "63495828bcd57200132d4615",
+    part_name: "Mid Speed Motor 1 Gear",
+    part_number: "254662178"
+  },
+  {
+    _id: "63495828bcd57200132d4616",
+    part_name: "Mid Speed Motor 1 Gear",
+    part_number: "238662132"
+  },
+  {
+    _id: "6530f55dab5d46a8f9d34b44",
+    part_name: "Wunderbaum - Black Ice",
+    part_number: "35685453"
+  },
+  {
+    _id: "6530f55dab5d46a8f9d34b45",
+    part_name: "Wunderbaum - Black Ice",
+    part_number: "46564655"
+  },
+  {
+    _id: "63495db7bcd57200132d54c1",
+    part_name: "Rear High Speed Motor 1 Gear",
+    part_number: "465613154"
+  },
+  {
+    _id: "63495db7bcd57200132d54c2",
+    part_name: "Front High Speed Motor 1 Gear",
+    part_number: "465613155"
+  },
+  {
+    _id: "63495db7bcd57200132d54c3",
+    part_name: "Front High Speed Motor 1 Gear",
+    part_number: "465613156"
+  },
+  {
+    _id: "63495db7bcd57200132d54x3",
+    part_name: "Wheel Nut",
+    part_number: ""
+  }
+];
+
+export const Default = {
+  args: {
+    flexDirection: "column",
+    flexWrap: "nowrap",
+    gates: [],
+    parts,
+    value: { _id: "", part_name: "", part_number: "" }
+  },
+
+  render: Template
+};
+
+export const SelectedPart = {
+  args: {
+    flexDirection: "column",
+    flexWrap: "nowrap",
+    gates: [],
+    parts,
+    value: {
+      _id: "",
+      part_name: "Mid Speed Motor 1 Gear",
+      part_number: "254662132"
+    }
+  },
+
+  render: Template
+};
---
+++ src/PartSelector/PartSelector.tsx
@@ -0,0 +1,87 @@
+import Autocomplete from "../Autocomplete";
+import { Box } from "@mui/material";
+import { PartSelectorProps } from "./PartSelector.types";
+import React from "react";
+import { uniqueSortedArray } from "../utils/common";
+
+// component to select a part
+function PartSelector({
+  flexDirection = "column",
+  flexWrap = "nowrap",
+  onChange = () => {},
+  size = "medium",
+  value = { _id: "", part_name: "", part_number: "" },
+  parts = []
+}: PartSelectorProps) {
+  // sort and get unique part names
+  const partNames = uniqueSortedArray(parts.map(part => part.part_name));
+
+  // get part numbers for the selected part name
+  const partNumbers = parts
+    .filter(part => part.part_name === value.part_name)
+    .map(part => part.part_number)
+    .filter(number => number !== "");
+
+  // create the selector components for part name and part number
+  return (
+    <Box
+      data-testid="part-select"
+      sx={{
+        display: "flex",
+        flexDirection,
+        flexWrap,
+        gap: flexDirection === "row" ? "0 24px" : 0
+      }}
+    >
+      <Box flex="40%">
+        <Autocomplete
+          label="Part Name"
+          required
+          multiple={false}
+          options={partNames}
+          onChange={(_event, partName) => {
+            const newValue = partName === null ? "" : partName;
+            // update onChange with the new part name
+            onChange({
+              _id: "",
+              part_name: newValue,
+              part_number: ""
+            });
+          }}
+          size={size}
+          value={value.part_name === "" ? null : value.part_name}
+        />
+      </Box>
+      <Box flex="40%">
+        <Autocomplete
+          disabled={value.part_name === null || value.part_name === ""}
+          label="Part Number"
+          required
+          multiple={false}
+          options={partNumbers}
+          onChange={(_event, partNumber) => {
+            const newValue = partNumber === null ? "" : partNumber;
+
+            // get the part id for the selected part number
+            const partId = parts.filter(
+              part =>
+                part.part_number === newValue &&
+                part.part_name === value.part_name
+            )[0]._id;
+
+            // update onChange with the new part number and part id
+            onChange({
+              _id: partId,
+              part_name: value.part_name,
+              part_number: newValue
+            });
+          }}
+          size={size}
+          value={value.part_number === "" ? null : value.part_number}
+        />
+      </Box>
+    </Box>
+  );
+}
+
+export default PartSelector;
