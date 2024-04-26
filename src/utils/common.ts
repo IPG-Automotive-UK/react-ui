52214843b53122e0134366a1f28410fe2f8c7f0c
syedsalehinipg <syed.salehin@ipg-automotive.uk>
2024-04-26 12:42:25 +0100

    Add uniqueSortedArray function to common.ts

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -4,3 +4,8 @@ import { KeyValueOption } from "../Common.types";
 export function isKeyValueOption(obj: unknown): obj is KeyValueOption {
   return !!obj && typeof obj === "object" && "key" in obj && "value" in obj;
 }
+
+// function to return unique sorted array of values from a string array
+export function uniqueSortedArray(values: string[]) {
+  return values.filter((item, i, ar) => ar.indexOf(item) === i).sort();
+}
