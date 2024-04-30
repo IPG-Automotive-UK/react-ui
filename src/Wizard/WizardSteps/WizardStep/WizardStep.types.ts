9a5ce9febfd12ba7618ee9f68d52ec08563654aa
lukemojo <143453762+lukemojo@users.noreply.github.com>
2024-04-30 15:39:45 +0100

    Update WizardStep to use only error and helperText to align with input error behaviour
    Update stories, tests and types

--- src/Wizard/WizardSteps/WizardStep/WizardStep.types.ts
+++ src/Wizard/WizardSteps/WizardStep/WizardStep.types.ts
@@ -1,5 +1,3 @@
-import { ReactNode } from "react";
-
 export type WizardStepProps = {
   /**
    * If true, the step is marked as completed. Is normally set by the WizardSteps parent component.
@@ -10,15 +8,10 @@ export type WizardStepProps = {
    */
   error?: boolean;
   /**
-   * The error text to display.
-   * If this prop is set, the helperText prop is ignored.
-   */
-  errorText?: ReactNode;
-  /**
    * The helper text to display.
    * If the errorText prop is set, this prop is ignored.
    */
-  helperText?: ReactNode;
+  helperText?: string;
   /**
    * The index of the step. Normally set by the WizardSteps parent component.
    */
