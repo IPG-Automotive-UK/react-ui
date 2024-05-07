ccfcb07747d664412a9568f427cbc3c4247795e2
Jega Sriskantha <75164177+jegasriskantha@users.noreply.github.com>
2024-05-07 10:27:28 +0100

    Update src/Wizard/WizardContent/WizardContent.tsx
    Co-authored-by: syedsalehinipg <143519265+syedsalehinipg@users.noreply.github.com>

--- src/Wizard/WizardContent/WizardContent.tsx
+++ src/Wizard/WizardContent/WizardContent.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { Box } from "@mui/material";
 import { WizardContentProps } from "./WizardContent.types";
 
-export default React.forwardRef(function WizardContent(
+export default React.forwardRef<HTMLDivElement, WizardContentProps>(
   { children }: WizardContentProps,
   ref
 ) {
