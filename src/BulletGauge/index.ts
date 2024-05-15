07b54063efbf0a02e28af18cbb6a612f0cb2f453
syedsalehinipg <syed.salehin@ipg-automotive.uk>
2024-05-15 11:40:27 +0100

    Add BulletGaugeClientOnly component for wrapper to support SSR

--- src/BulletGauge/index.ts
+++ src/BulletGauge/index.ts
@@ -1,2 +1,9 @@
-export { default } from "./BulletGauge";
+/**
+ * Export the default export from the BulletGaugeClientOnly module.
+ */
+export { default } from "./BulletGaugeClientOnly";
+
+/**
+ * Export the BulletGaugeProps type from the BulletGauge.types module.
+ */
 export type { BulletGaugeProps } from "./BulletGauge.types";
