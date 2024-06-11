655c71f918381f515cda286a218bd1253055b653
Manjesh M Pillai <56511816+m4manjesh@users.noreply.github.com>
2024-06-11 17:06:44 +0100

    removing unused import

--- src/FileUploader/FileUploader.test.tsx
+++ src/FileUploader/FileUploader.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, test, vi } from "vitest";
+import { describe, expect, test } from "vitest";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 
 import FileUploader from "./FileUploader";
