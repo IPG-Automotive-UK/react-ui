ae46239a5e5be4b03819c82b87799e7d9cc4ac0f
Petauskas, Áron <67595415+AronPetrauskas@users.noreply.github.com>
2024-06-13 16:59:10 +0200

    remove unused vars

--- src/CardContent/Infographic/Infographic.test.tsx
+++ src/CardContent/Infographic/Infographic.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, vi } from "vitest";
+import { describe, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 
 import Infographic from "./Infographic";
