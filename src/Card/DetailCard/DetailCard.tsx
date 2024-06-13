d6cdaac6968369f79a0260a30c5fc47810e9761d
Petauskas, Áron <67595415+AronPetrauskas@users.noreply.github.com>
2024-06-13 17:50:57 +0200

    remove unnecessary fragments, add id for easier testing

--- src/Card/DetailCard/DetailCard.tsx
+++ src/Card/DetailCard/DetailCard.tsx
@@ -30,58 +30,57 @@ function DetailCard({
 }: DetailCardProps) {
   // render the detail card
   return (
-    <Fragment>
-      <Stack
-        mt={1}
-        mb={3}
+    <Stack
+      id="DetailCard"
+      mt={1}
+      mb={3}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        flexGrow: 1,
+        height: "100%",
+        width
+      }}
+    >
+      <DetailCardHeader
+        title={title}
+        subtitle={subtitle}
+        buttonsStack={buttonsStack}
+        labels={labels}
+        width={width}
+        onClickLabel={onClickLabel}
+      />
+      <Box
         sx={{
           display: "flex",
-          flexDirection: "column",
-          flexGrow: 1,
+          flexDirection: "row",
           height: "100%",
-          width
+          my: 2,
+          overflow: "auto"
         }}
       >
-        <DetailCardHeader
-          title={title}
-          subtitle={subtitle}
-          buttonsStack={buttonsStack}
-          labels={labels}
-          width={width}
-          onClickLabel={onClickLabel}
-        />
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            height: "100%",
-            my: 2,
-            overflow: "auto"
-          }}
-        >
-          <Box ml={0.5}>
-            <Card>
-              <Infographic media={media} />
-              <FileDetails
-                files={files}
-                downloadButtonText={downloadButtonText}
-                downloadButtonTextOnSearch={downloadButtonTextOnSearch}
-                fileTitle={fileTitle}
-                onClickDownload={onClickDownload}
-                onClickFile={onClickFile}
-              />
-            </Card>
-          </Box>
-          <Stack
-            ml={2}
-            spacing={2}
-            sx={{ display: "flex", flexGrow: 1, mr: 0.5, width: "100%" }}
-          >
-            {content}
-          </Stack>
+        <Box ml={0.5}>
+          <Card>
+            <Infographic media={media} />
+            <FileDetails
+              files={files}
+              downloadButtonText={downloadButtonText}
+              downloadButtonTextOnSearch={downloadButtonTextOnSearch}
+              fileTitle={fileTitle}
+              onClickDownload={onClickDownload}
+              onClickFile={onClickFile}
+            />
+          </Card>
         </Box>
-      </Stack>
-    </Fragment>
+        <Stack
+          ml={2}
+          spacing={2}
+          sx={{ display: "flex", flexGrow: 1, mr: 0.5, width: "100%" }}
+        >
+          {content}
+        </Stack>
+      </Box>
+    </Stack>
   );
 }
 
@@ -166,7 +165,7 @@ function DetailCardHeader({
         </Box>
       </Box>
       {labels && labels.length > 0 && (
-        <LableStack
+        <LabelStack
           labelSpacing={labelSpacing}
           width={width}
           labels={labels}
@@ -177,7 +176,7 @@ function DetailCardHeader({
   );
 }
 
-function LableStack({
+function LabelStack({
   labelSpacing,
   width,
   labels,
--- src/CardContent/Infographic/Infographic.tsx
+++ src/CardContent/Infographic/Infographic.tsx
@@ -1,44 +1,43 @@
 import { Box, CardMedia } from "@mui/material";
-import React, { Fragment } from "react";
 
 import { InfographicProps } from "./Infographic.types";
+import React from "react";
 
 const Infographic = ({ media, versionChip }: InfographicProps) => {
   return (
-    <Fragment>
-      <Box
-        position={"relative"}
+    <Box
+      id="Infographic"
+      position={"relative"}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        width: 368
+      }}
+    >
+      <CardMedia
+        component="img"
+        src={media}
         sx={{
-          display: "flex",
-          justifyContent: "center",
-          width: 368
+          boxSizing: "content-box",
+          height: 190,
+          objectFit: "contain",
+          padding: 2,
+          width: 336
+        }}
+      />
+      <Box
+        position={"absolute"}
+        display={"flex"}
+        alignItems={"end"}
+        sx={{
+          height: 190,
+          padding: 2,
+          width: 336
         }}
       >
-        <CardMedia
-          component="img"
-          src={media}
-          sx={{
-            boxSizing: "content-box",
-            height: 190,
-            objectFit: "contain",
-            padding: 2,
-            width: 336
-          }}
-        />
-        <Box
-          position={"absolute"}
-          display={"flex"}
-          alignItems={"end"}
-          sx={{
-            height: 190,
-            padding: 2,
-            width: 336
-          }}
-        >
-          <Box padding={1}>{versionChip}</Box>
-        </Box>
+        <Box padding={1}>{versionChip}</Box>
       </Box>
-    </Fragment>
+    </Box>
   );
 };
 
