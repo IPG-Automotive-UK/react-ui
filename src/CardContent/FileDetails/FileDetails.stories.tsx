0b2e60d05789d27731d20b7c094da9d460d08b90
Petauskas, Áron <67595415+AronPetrauskas@users.noreply.github.com>
2024-06-13 16:58:39 +0200

    fix stories category name

--- src/CardContent/FileDetails/FileDetails.stories.tsx
+++ src/CardContent/FileDetails/FileDetails.stories.tsx
@@ -11,7 +11,7 @@ import React from "react";
  */
 const meta: Meta<typeof FileDetails> = {
   component: FileDetails,
-  title: "CardContents/FileDetails"
+  title: "CardContent/FileDetails"
 };
 export default meta;
 
