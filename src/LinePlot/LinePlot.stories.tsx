b64ffe8b407c91018ee8c13f22ff6c2ce51c1760
David Barry <david.barry@ipg-automotive.com>
2024-07-08 16:49:42 +0100

    New story to show axis labels with scientific notation

--- src/LinePlot/LinePlot.stories.tsx
+++ src/LinePlot/LinePlot.stories.tsx
@@ -28,7 +28,22 @@ export const Default = {
     xdata: [0, 1, 2, 3, 4, 5],
     xlabel: "X label (rad)",
     ydata: [0, 20, 30, 40, 50],
-    ylabel: "Y label (-)"
+    ylabel: "Y label (Nm)"
+  },
+  render: Template
+};
+
+// Scientific Notation
+export const ScientificNotation = {
+  args: {
+    showGrid: true,
+    showMarkers: true,
+    showTitle: true,
+    title: "Scientific Notation",
+    xdata: [0, 0.0001, 0.0002, 0.0003, 0.0004, 0.0005],
+    xlabel: "X label (rad)",
+    ydata: [0, 1, 200, 3000, 40000, 50000],
+    ylabel: "Y label (Nm)"
   },
   render: Template
 };
