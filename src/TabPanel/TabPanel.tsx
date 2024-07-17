6ee46bf339d9005c935ee44448ad78b67ff16e20
Joseph Meyrick <joseph.s.meyrick@gmail.com>
2024-07-17 15:13:08 +0100

    Revert "tab panel background colour"
    This reverts commit 9733572b080451625ab45b441200ecae8c1abbdc.

--- src/TabPanel/TabPanel.tsx
+++ src/TabPanel/TabPanel.tsx
@@ -102,6 +102,7 @@ const TabPanel = ({
               aria-labelledby={`tab-${index}`}
               display={"flex"}
               flexDirection={"column"}
+              height={activeTab === index ? 1 : 0}
             >
               {activeTab === index && child.props.children}
             </Box>
