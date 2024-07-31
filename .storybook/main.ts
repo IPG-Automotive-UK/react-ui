20262198798b3d2d5d7d5b6e9a40996b8ad56f78
Matthew Corner <matt.corner@ipg-automotive.com>
2024-07-31 10:47:35 +0100

    add dark mode styling for preview and loading containers (#915)
    * add dark mode styling for preview and loading containers
    
    * Disable backgrounds
    
    * Latest storybook
    
    * Fix import error

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,9 +12,7 @@ const config: StorybookConfig = {
     name: "@storybook/react-vite",
     options: {}
   },
-  docs: {
-    autodocs: true
-  },
+  docs: {},
   typescript: {
     reactDocgen: "react-docgen-typescript",
     reactDocgenTypescriptOptions: {
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,3 +1,5 @@
+import "./styles.css";
+
 import { DARK_MODE_EVENT_NAME } from "storybook-dark-mode";
 import { DocsContainer } from "@storybook/addon-docs";
 import React from "react";
@@ -5,12 +7,11 @@ import { addons } from "@storybook/preview-api";
 import { themeProviderDecorator } from "./decorators";
 import { themes } from "@storybook/theming";
 
-const channel = addons.getChannel();
-
 export const decorators = [themeProviderDecorator];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: { disable: true },
   controls: {
     expanded: true, // Adds the description and default columns
     matchers: {
@@ -21,16 +22,23 @@ export const parameters = {
   },
   darkMode: {
     dark: { ...themes.dark },
-    light: { ...themes.light }
+    light: { ...themes.light },
+    stylePreview: true
   },
   docs: {
     container: props => {
-      const [isDark, setDark] = React.useState();
+      const [isDark, setDark] = React.useState(() => {
+        // NOTE: This is a bit of a hack to get the initial value of dark mode from localStorage. It relies on the internal implementation of the storybook-dark-mode addon. This is because the exposed useDarkMode hook cannot be used in a decorator like this. https://github.com/hipstersmoothie/storybook-dark-mode/issues/282
+        const storedItem = window.localStorage.getItem("sb-addon-themes-3");
+        const stored = JSON.parse(storedItem!);
+        return stored.current === "dark";
+      });
 
       React.useEffect(() => {
+        const channel = addons.getChannel();
         channel.on(DARK_MODE_EVENT_NAME, setDark);
         return () => channel.removeListener(DARK_MODE_EVENT_NAME, setDark);
-      }, [channel, setDark]);
+      }, [setDark]);
 
       return (
         <DocsContainer {...props} theme={isDark ? themes.dark : themes.light} />
@@ -38,3 +46,4 @@ export const parameters = {
     }
   }
 };
+export const tags = ["autodocs"];
