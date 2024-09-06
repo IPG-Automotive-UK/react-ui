110b0f2bdf3b229bf14150c999058b64f6a59a23
lukemojo <143453762+lukemojo@users.noreply.github.com>
2024-09-06 09:20:49 +0100

    v8.7.1 release (#936)
    * 8.7.1
    
    * fix the close button alignment
    
    * textAlign: right not needed at all. Removed unwanted box as well
    
    * TD-2804-Cannot-build-CRA-based-apps-with-latest-RUI (#935)
    
    * comment-out-exports
    
    Commented out 4 recent components that were causing build errors.
    
    * Update to Pr comments
    
    Moved commented out files tot he end of the file, added description why with a link to the ticket.
    
    * Moved kona and react-konva to dev dependencies
    
    * 8.7.1-0
    
    * Comment out Figure component to resolve react-konva build issue (TD-2804) (#938)
    
    This PR addresses an additional build issue discovered during pre-release testing, related to the Figure component which uses react-konva. This component was missed in the initial fix for TD-2804.
    
    * 8.7.1-1
    
    ---------
    
    Co-authored-by: Manjesh M Pillai <m4manjesh@gmail.com>
    Co-authored-by: Manjesh M Pillai <56511816+m4manjesh@users.noreply.github.com>
    Co-authored-by: stevenmcsorleyipg <steven.mcsorley@ipg-automotive.uk>

--- src/index.ts
+++ src/index.ts
@@ -27,10 +27,6 @@ export {
 export { default as Canvas, type CanvasProps } from "./Canvas";
 export { default as CanvasItem, type CanvasItemProps } from "./CanvasItem";
 export { default as EditLabelDialog } from "./EditLabelDialog";
-export { default as RoadMarking, type RoadMarkingProps } from "./RoadMarking";
-export { default as RoadSurface, type RoadSurfaceProps } from "./RoadSurface";
-export { default as TrafficSign, type TrafficSignProps } from "./TrafficSign";
-export { default as VehiclePath, type VehiclePathProps } from "./VehiclePath";
 export { default as Breadcrumbs, type BreadcrumbsProps } from "./Breadcrumbs";
 export {
   DetailCard,
@@ -51,11 +47,6 @@ export { default as Checkbox, type CheckboxProps } from "./Checkbox";
 export { default as ClientOnly, type ClientOnlyProps } from "./ClientOnly";
 export { default as Copyright, type CopyrightProps } from "./Copyright";
 export { default as Color, type ColorProps } from "./Color";
-export { default as Figure, type FigureProps } from "./Figure";
-export {
-  default as TrafficLight,
-  type TrafficLightProps
-} from "./TrafficLight";
 export {
   default as DeletableList,
   type DeletableListProps
@@ -266,3 +257,18 @@ export {
   default as UploaderHeader,
   type UploaderHeaderProps
 } from "./Uploader";
+/*
+  The following components are temporarily commented out to prevent CRA build failures.
+  For more details, refer to the related ticket:
+
+  Ticket: https://sce.myjetbrains.com/youtrack/issue/TD-2804/Cannot-build-CRA-based-apps-with-latest-RUI
+*/
+// export { default as Figure, type FigureProps } from "./Figure";
+// export {
+//   default as TrafficLight,
+//   type TrafficLightProps
+// } from "./TrafficLight";
+// export { default as RoadMarking, type RoadMarkingProps } from "./RoadMarking";
+// export { default as RoadSurface, type RoadSurfaceProps } from "./RoadSurface";
+// export { default as TrafficSign, type TrafficSignProps } from "./TrafficSign";
+// export { default as VehiclePath, type VehiclePathProps } from "./VehiclePath";
