9bf5e5b6e4a65ced450ed791a355478341d91691
arpadlomenipg <arpad.lomen@ipg-automotive.com>
2024-09-17 14:32:42 +0200

    lazy load Infographic component

--- src/CardContent/Infographic/Infographic.tsx
+++ src/CardContent/Infographic/Infographic.tsx
@@ -1,36 +1,62 @@
-import { Box } from "@mui/material";
+import { Box, CardMedia } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
+
 import { InfographicProps } from "./Infographic.types";
-import LazyLoadCardMedia from "../../LazyLoadImage/LazyLoadCardMedia/LazyLoadCardMedia";
-import React from "react";
 import VersionChip from "../../VersionChip/VersionChip";
 
 const Infographic = ({ media, version }: InfographicProps) => {
+  // state to track if the image is visible
+  const [isVisible, setIsVisible] = useState(false);
+
+  // reference to the bounding box of the infographic
+  const boxRef = useRef(null);
+
+  // when the image is in view, set isVisible to true
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        if (entries[0].isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect();
+        }
+      },
+      { threshold: 0.1 }
+    );
+    if (boxRef.current) {
+      observer.observe(boxRef.current);
+    }
+    return () => observer.disconnect();
+  }, []);
+
+  // render the card infographic content
+  // CardMedia and the version chip is rendered conditionally
   return (
     <Box
       className="infographic-container"
       position={"relative"}
+      ref={boxRef}
       sx={{
         display: "flex",
         justifyContent: "center",
         width: 368
       }}
     >
-      <LazyLoadCardMedia
-        CardMediaProps={{
-          style: {
-            boxSizing: "content-box",
-            height: 190,
-            objectFit: "contain",
-            padding: 200,
-            width: 336
-          }
+      <CardMedia
+        component="img"
+        image={isVisible ? media : undefined}
+        sx={{
+          boxSizing: "content-box",
+          display: isVisible ? "block" : "none",
+          height: 190,
+          objectFit: "contain",
+          padding: 2,
+          width: 336
         }}
-        src={media}
       />
       {version ? (
         <Box
           position={"absolute"}
-          display={"flex"}
+          display={isVisible ? "flex" : "none"}
           alignItems={"end"}
           sx={{
             height: 190,
