f675462e9a9bf9ee426e97e6ed337656966866a5
lukemojo <143453762+lukemojo@users.noreply.github.com>
2024-10-17 19:56:23 +0100

    Update JSDOC for NumberFieldProps

--- src/NumberField/NumberField.types.ts
+++ src/NumberField/NumberField.types.ts
@@ -17,13 +17,40 @@ export type NumberFieldProps = Omit<
   TextFieldProps,
   "multiline" | "onChange" | "rows" | "select" | "sx" | "type"
 > & {
+  /**
+   * The end adornment for the input.
+   */
   endAdornment?: string;
+  /**
+   * The min validation value for the input.
+   */
   min?: number;
+  /**
+   * The max validation value for the input.
+   */
   max?: number;
+  /**
+   * Callback fired when the value changes, returns event object with number value.
+   */
   onChange?: (event: NumberFieldChangeEvent) => void;
+  /**
+   * Shows a min/max error message when the value is out of range.
+   */
   showMinMaxErrorMessage?: boolean;
+  /**
+   * The start adornment for the input.
+   */
   startAdornment?: string;
+  /**
+   * The step increment of the input.
+   */
   step?: number;
+  /**
+   * Show/hide the input stepper buttons.
+   */
   stepper?: boolean;
+  /**
+   * The numeric value of the input.
+   */
   value?: number | null;
 };
