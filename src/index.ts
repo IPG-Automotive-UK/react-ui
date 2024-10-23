4155d54f7cef3b8d02749537b58da01e31ddb520
arpadlomenipg <arpad.lomen@ipg-automotive.com>
2024-10-23 15:42:33 +0200

    change the exports

--- src/index.ts
+++ src/index.ts
@@ -1,211 +1,178 @@
+export { default as ActionDialog } from "./ActionDialog/index.js";
+export { type ActionDialogProps } from "./ActionDialog";
+export { default as AlignHorizontal } from "./AlignHorizontal/index.js";
+export { type AlignHorizontalProps } from "./AlignHorizontal";
+export { default as AlignVertical } from "./AlignVertical/index.js";
+export { type AlignVerticalProps } from "./AlignVertical";
+export { default as VirtoAppHeader } from "./VirtoAppHeader/index.js";
+export { type VirtoAppHeaderProps } from "./VirtoAppHeader";
+export { default as VirtoAppLayout } from "./VirtoAppLayout/index.js";
+export { type VirtoAppLayoutProps } from "./VirtoAppLayout";
+export { default as AppHeader } from "./AppHeader/index.js";
+export { type AppHeaderProps } from "./AppHeader";
+export { default as AppLayout } from "./AppLayout/index.js";
+export { type AppLayoutProps } from "./AppLayout";
+export { default as Autocomplete } from "./Autocomplete/index.js";
+export { type AutocompleteProps } from "./Autocomplete";
+export { default as Canvas } from "./Canvas/index.js";
+export { type CanvasProps } from "./Canvas";
+export { default as CanvasItem } from "./CanvasItem/index.js";
+export { type CanvasItemProps } from "./CanvasItem";
+export { default as EditLabelDialog } from "./EditLabelDialog/index.js";
+export { RoadPreview } from "./RoadPreview/index.js";
+export { type RoadPreviewProps } from "./RoadPreview";
+export { default as Breadcrumbs } from "./Breadcrumbs/index.js";
+export { type BreadcrumbsProps } from "./Breadcrumbs";
+export { DetailCard, SummaryCard, TableCard } from "./Card/index.js";
 export {
-  default as ActionDialog,
-  type ActionDialogProps
-} from "./ActionDialog";
-export {
-  default as AlignHorizontal,
-  type AlignHorizontalProps
-} from "./AlignHorizontal";
-export {
-  default as AlignVertical,
-  type AlignVerticalProps
-} from "./AlignVertical";
-export {
-  default as VirtoAppHeader,
-  type VirtoAppHeaderProps
-} from "./VirtoAppHeader";
-export {
-  default as VirtoAppLayout,
-  type VirtoAppLayoutProps
-} from "./VirtoAppLayout";
-export { default as AppHeader, type AppHeaderProps } from "./AppHeader";
-export { default as AppLayout, type AppLayoutProps } from "./AppLayout";
-export {
-  default as Autocomplete,
-  type AutocompleteProps
-} from "./Autocomplete";
-export { default as Canvas, type CanvasProps } from "./Canvas";
-export { default as CanvasItem, type CanvasItemProps } from "./CanvasItem";
-export { default as EditLabelDialog } from "./EditLabelDialog";
-export { RoadPreview, type RoadPreviewProps } from "./RoadPreview";
-export { default as Breadcrumbs, type BreadcrumbsProps } from "./Breadcrumbs";
-export {
-  DetailCard,
-  SummaryCard,
-  TableCard,
   type DetailCardProps,
   type DetailCardHeaderProps,
   type SummaryCardProps,
   type TableCardProps
 } from "./Card";
-export {
-  Infographic,
-  FileDetails,
-  type InfographicProps,
-  type FileDetailsProps
-} from "./CardContent";
-export { default as Checkbox, type CheckboxProps } from "./Checkbox";
-export { default as ClientOnly, type ClientOnlyProps } from "./ClientOnly";
-export { default as Copyright, type CopyrightProps } from "./Copyright";
-export { default as Color, type ColorProps } from "./Color";
-export {
-  default as DeletableList,
-  type DeletableListProps
-} from "./DeletableList";
-export { default as DialogTitle, type DialogTitleProps } from "./DialogTitle";
-export {
-  default as FeedbackForm,
-  type FeedbackFormProps
-} from "./FeedbackForm";
-export { default as VersionChip, type VersionChipProps } from "./VersionChip";
-export {
-  default as FileUploader,
-  type FileUploaderProps
-} from "./FileUploader";
+export { Infographic, FileDetails } from "./CardContent/index.js";
+export { type InfographicProps, type FileDetailsProps } from "./CardContent";
+export { default as Checkbox } from "./Checkbox/index.js";
+export { type CheckboxProps } from "./Checkbox";
+export { default as ClientOnly } from "./ClientOnly/index.js";
+export { type ClientOnlyProps } from "./ClientOnly";
+export { default as Copyright } from "./Copyright/index.js";
+export { type CopyrightProps } from "./Copyright";
+export { default as Color } from "./Color/index.js";
+export { type ColorProps } from "./Color";
+export { default as DeletableList } from "./DeletableList/index.js";
+export { type DeletableListProps } from "./DeletableList";
+export { default as DialogTitle } from "./DialogTitle/index.js";
+export { type DialogTitleProps } from "./DialogTitle";
+export { default as FeedbackForm } from "./FeedbackForm/index.js";
+export { type FeedbackFormProps } from "./FeedbackForm";
+export { default as VersionChip } from "./VersionChip/index.js";
+export { type VersionChipProps } from "./VersionChip";
+export { default as FileUploader } from "./FileUploader/index.js";
+export { type FileUploaderProps } from "./FileUploader";
 export {
   CheckboxFilter,
   FilterPopper,
   FilterStack,
   LabelFilter,
-  RangeFilter,
+  RangeFilter
+} from "./Filter/index.js";
+export {
   type CheckboxFilterProps,
   type FilterPopperProps,
   type FilterStackProps,
   type LabelFilterProps,
   type RangeFilterProps
 } from "./Filter";
-export { default as FontPicker, type FontPickerProps } from "./FontPicker";
-export { default as FontStyle, type FontStyleProps } from "./FontStyle";
-export {
-  default as ImageUploader,
-  type ImageUploaderProps
-} from "./ImageUploader";
+export { default as FontPicker } from "./FontPicker/index.js";
+export { type FontPickerProps } from "./FontPicker";
+export { default as FontStyle } from "./FontStyle/index.js";
+export { type FontStyleProps } from "./FontStyle";
+export { default as ImageUploader } from "./ImageUploader/index.js";
+export { type ImageUploaderProps } from "./ImageUploader";
 export {
   LabelChip,
   LabelChipGroup,
-  default as LabelSelector,
+  default as LabelSelector
+} from "./LabelSelector/index.js";
+export {
   type LabelChipProps,
   type LabelChipGroupProps,
   type LabelSelectorProps
 } from "./LabelSelector";
-export {
-  default as LazyLoadImage,
-  type LazyLoadImageProps
-} from "./LazyLoadImage";
-export { default as TabPanel, type TabPanelProps } from "./TabPanel";
-export { default as LinePlot, type LinePlotProps } from "./LinePlot";
-export { default as SurfacePlot, type SurfacePlotProps } from "./SurfacePlot";
-export { default as BulletGauge, type BulletGaugeProps } from "./BulletGauge";
-export { default as Loading, type LoadingProps } from "./Loading";
-export { default as LoginForm, type LoginFormProps } from "./LoginForm";
-export { default as ModelButton, type ModelButtonProps } from "./ModelButton";
-export { default as MultiColor, type MultiColorProps } from "./MultiColor";
-export {
-  default as ConditionalDialog,
-  type ConditionalDialogProps
-} from "./ConditionalDialog";
-export {
-  default as LoadErrorMessage,
-  type LoadErrorMessageProps
-} from "./LoadErrorMessage";
-export {
-  default as MultiLabelPopover,
-  type MultiLabelPopoverProps
-} from "./MultiLabelPopover";
-export { default as MultiText, type MultiTextProps } from "./MultiText";
-export { default as NumberField, type NumberFieldProps } from "./NumberField";
-export {
-  default as PasswordChangeDialog,
-  type PasswordChangeDialogProps
-} from "./PasswordChangeDialog";
-export {
-  default as PasswordChangeForm,
-  type PasswordChangeFormProps
-} from "./PasswordChangeForm";
-export {
-  default as PasswordResetForm,
-  type PasswordResetFormProps
-} from "./PasswordResetForm";
-export {
-  default as PartSelector,
-  type PartSelectorProps
-} from "./PartSelector";
-export {
-  default as RadioButtons,
-  type RadioButtonsProps
-} from "./RadioButtons";
-export {
-  default as SelectorButton,
-  type SelectorButtonProps
-} from "./SelectorButton";
-export {
-  default as RegistrationForm,
-  type RegistrationFormProps
-} from "./RegistrationForm";
-export { default as SearchBar, type SearchBarProps } from "./SearchBar";
-export { default as Select, type SelectProps } from "./Select";
-export {
-  Sidebar,
-  SidebarDivider,
-  SidebarItem,
-  type SidebarProps,
-  type SidebarItemProps
-} from "./Sidebar";
-export { default as AppLauncher, type AppLauncherProps } from "./AppLauncher";
-export { default as Slider, type SliderProps } from "./Slider";
-export { default as Snackbar, type SnackbarProps } from "./Snackbar";
-export {
-  SnackbarProvider,
-  useSnackbar,
-  type SnackbarProviderProps
-} from "./SnackbarProvider";
+export { default as LazyLoadImage } from "./LazyLoadImage/index.js";
+export { type LazyLoadImageProps } from "./LazyLoadImage";
+export { default as TabPanel } from "./TabPanel/index.js";
+export { type TabPanelProps } from "./TabPanel";
+export { default as LinePlot } from "./LinePlot/index.js";
+export { type LinePlotProps } from "./LinePlot";
+export { default as SurfacePlot } from "./SurfacePlot/index.js";
+export { type SurfacePlotProps } from "./SurfacePlot";
+export { default as BulletGauge } from "./BulletGauge/index.js";
+export { type BulletGaugeProps } from "./BulletGauge";
+export { default as Loading } from "./Loading/index.js";
+export { type LoadingProps } from "./Loading";
+export { default as LoginForm } from "./LoginForm/index.js";
+export { type LoginFormProps } from "./LoginForm";
+export { default as ModelButton } from "./ModelButton/index.js";
+export { type ModelButtonProps } from "./ModelButton";
+export { default as MultiColor } from "./MultiColor/index.js";
+export { type MultiColorProps } from "./MultiColor";
+export { default as ConditionalDialog } from "./ConditionalDialog/index.js";
+export { type ConditionalDialogProps } from "./ConditionalDialog";
+export { default as LoadErrorMessage } from "./LoadErrorMessage/index.js";
+export { type LoadErrorMessageProps } from "./LoadErrorMessage";
+export { default as MultiLabelPopover } from "./MultiLabelPopover/index.js";
+export { type MultiLabelPopoverProps } from "./MultiLabelPopover";
+export { default as MultiText } from "./MultiText/index.js";
+export { type MultiTextProps } from "./MultiText";
+export { default as NumberField } from "./NumberField/index.js";
+export { type NumberFieldProps } from "./NumberField";
+export { default as PasswordChangeDialog } from "./PasswordChangeDialog/index.js";
+export { type PasswordChangeDialogProps } from "./PasswordChangeDialog";
+export { default as PasswordChangeForm } from "./PasswordChangeForm/index.js";
+export { type PasswordChangeFormProps } from "./PasswordChangeForm";
+export { default as PasswordResetForm } from "./PasswordResetForm/index.js";
+export { type PasswordResetFormProps } from "./PasswordResetForm";
+export { default as PartSelector } from "./PartSelector/index.js";
+export { type PartSelectorProps } from "./PartSelector";
+export { default as RadioButtons } from "./RadioButtons/index.js";
+export { type RadioButtonsProps } from "./RadioButtons";
+export { default as SelectorButton } from "./SelectorButton/index.js";
+export { type SelectorButtonProps } from "./SelectorButton";
+export { default as RegistrationForm } from "./RegistrationForm/index.js";
+export { type RegistrationFormProps } from "./RegistrationForm";
+export { default as SearchBar } from "./SearchBar/index.js";
+export { type SearchBarProps } from "./SearchBar";
+export { default as Select } from "./Select/index.js";
+export { type SelectProps } from "./Select";
+export { Sidebar, SidebarDivider, SidebarItem } from "./Sidebar/index.js";
+export { type SidebarProps, type SidebarItemProps } from "./Sidebar";
+export { default as AppLauncher } from "./AppLauncher/index.js";
+export { type AppLauncherProps } from "./AppLauncher";
+export { default as Slider } from "./Slider/index.js";
+export { type SliderProps } from "./Slider";
+export { default as Snackbar } from "./Snackbar/index.js";
+export { type SnackbarProps } from "./Snackbar";
+export { SnackbarProvider, useSnackbar } from "./SnackbarProvider/index.js";
+export { type SnackbarProviderProps } from "./SnackbarProvider";
 export {
   StatusCard,
   StatusIcon,
   StatusLabel,
   StatusCountBar,
+  StatusCountTable
+} from "./Status/index.js";
+export {
   type StatusCountBarProps,
-  StatusCountTable,
   type StatusCardProps,
   type StatusIconProps,
   type StatusLabelProps,
   type StatusCountTableProps
 } from "./Status";
-export { default as SwitchField, type SwitchFieldProps } from "./SwitchField";
-export { default as TextField, type TextFieldProps } from "./TextField";
-export {
-  ThemeProvider,
-  type ThemeProviderProps,
-  useTheme
-} from "./ThemeProvider";
-export {
-  default as ToggleColorMode,
-  type ToggleColorModeProps
-} from "./ToggleColorMode";
-export {
-  default as TransferList,
-  type TransferListProps
-} from "./TransferList";
-export {
-  default as TreeViewList,
-  type TreeViewListProps
-} from "./TreeViewList";
-export { default as TruncatedTooltip } from "./TruncatedTooltip";
-export { default as UserMenu, type UserMenuProps } from "./UserMenu";
-export { UserAvatar, useColorMap, type UserAvatarProps } from "./UserAvatar";
-export {
-  default as NoWrapTypography,
-  type NoWrapTypographyProps
-} from "./NoWrapTypography";
-export {
-  default as ModelButtonImage,
-  type ModelButtonImageProps
-} from "./ModelButtonImage";
-export {
-  ConfirmProvider,
-  useConfirm,
-  type ConfirmProviderProps
-} from "./ConfirmProvider";
+export { default as SwitchField } from "./SwitchField/index.js";
+export { type SwitchFieldProps } from "./SwitchField";
+export { default as TextField } from "./TextField/index.js";
+export { type TextFieldProps } from "./TextField";
+export { ThemeProvider, useTheme } from "./ThemeProvider/index.js";
+export { type ThemeProviderProps } from "./ThemeProvider";
+export { default as ToggleColorMode } from "./ToggleColorMode/index.js";
+export { type ToggleColorModeProps } from "./ToggleColorMode";
+export { default as TransferList } from "./TransferList/index.js";
+export { type TransferListProps } from "./TransferList";
+export { default as TreeViewList } from "./TreeViewList/index.js";
+export { type TreeViewListProps } from "./TreeViewList";
+export { default as TruncatedTooltip } from "./TruncatedTooltip/index.js";
+export { default as UserMenu } from "./UserMenu/index.js";
+export { type UserMenuProps } from "./UserMenu";
+export { UserAvatar, useColorMap } from "./UserAvatar/index.js";
+export { type UserAvatarProps } from "./UserAvatar";
+export { default as NoWrapTypography } from "./NoWrapTypography/index.js";
+export { type NoWrapTypographyProps } from "./NoWrapTypography";
+export { default as ModelButtonImage } from "./ModelButtonImage/index.js";
+export { type ModelButtonImageProps } from "./ModelButtonImage";
+export { ConfirmProvider, useConfirm } from "./ConfirmProvider/index.js";
+export { type ConfirmProviderProps } from "./ConfirmProvider";
 export {
   IpgLogo,
   VirtoBuild,
@@ -221,7 +188,9 @@ export {
   VirtoThumbsUpLeft,
   VirtoVehicle,
   CarMakerLogo,
-  AsamLogo,
+  AsamLogo
+} from "./SvgIcons/index.js";
+export {
   type IpgLogoProps,
   type VirtoBuildProps,
   type VirtoDataProps,
@@ -238,22 +207,14 @@ export {
   type CarMakerLogoProps,
   type AsamLogoProps
 } from "./SvgIcons";
-export {
-  default as VehicleSelect,
-  type VehicleSelectProps
-} from "./VehicleSelect";
-export {
-  default as VehicleSelector,
-  type VehicleSelectorProps
-} from "./VehicleSelector";
-export {
-  default as VehicleSelectDialog,
-  type VehicleSelectDialogProps
-} from "./VehicleSelectDialog";
-export {
-  default as ViewToggleButton,
-  type ViewToggleButtonProps
-} from "./ViewToggleButton";
+export { default as VehicleSelect } from "./VehicleSelect/index.js";
+export { type VehicleSelectProps } from "./VehicleSelect";
+export { default as VehicleSelector } from "./VehicleSelector/index.js";
+export { type VehicleSelectorProps } from "./VehicleSelector";
+export { default as VehicleSelectDialog } from "./VehicleSelectDialog/index.js";
+export { type VehicleSelectDialogProps } from "./VehicleSelectDialog";
+export { default as ViewToggleButton } from "./ViewToggleButton/index.js";
+export { type ViewToggleButtonProps } from "./ViewToggleButton";
 export {
   BackButton,
   CancelButton,
@@ -262,7 +223,9 @@ export {
   WizardActions,
   WizardContent,
   WizardStep,
-  WizardSteps,
+  WizardSteps
+} from "./Wizard/index.js";
+export {
   type WizardActionButtonProps,
   type WizardActionsProps,
   type WizardContentProps,
@@ -270,11 +233,9 @@ export {
   type WizardStepProps,
   type WizardStepsProps
 } from "./Wizard";
-export {
-  default as UploaderHeader,
-  type UploaderHeaderProps
-} from "./Uploader";
-export { useDelayedHover } from "./hover";
+export { default as UploaderHeader } from "./Uploader/index.js";
+export { type UploaderHeaderProps } from "./Uploader";
+export { useDelayedHover } from "./hover/index.js";
 
 /*
   The following components are temporarily commented out to prevent CRA build failures.
