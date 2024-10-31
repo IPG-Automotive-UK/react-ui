cdc279332deb6309af8b92fad49e481104df0772
lukemojo <143453762+lukemojo@users.noreply.github.com>
2024-10-31 09:27:00 +0000

    Add aria-label to Search

--- src/SearchBar/SearchBar.tsx
+++ src/SearchBar/SearchBar.tsx
@@ -25,6 +25,7 @@ export default function SearchBar({
       }}
     >
       <InputBase
+        aria-label={"Search"}
         placeholder={placeholder}
         value={value}
         onChange={onChange}
