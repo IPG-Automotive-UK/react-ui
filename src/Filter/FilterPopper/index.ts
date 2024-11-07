fa4ef6b3804d17123bb2f58328cfc424b167b1ba
Manjesh M Pillai <m4manjesh@gmail.com>
2024-11-07 15:25:12 +0000

    FilterPopper and FilterStack removed

--- src/Filter/FilterPopper/index.ts
+++
@@ -1,9 +0,0 @@
-import FilterPopper from "./FilterPopper";
-
-export type FilterPopperProps = {
-  children?: React.ReactNode;
-  count?: number;
-  label?: string;
-};
-
-export default FilterPopper as React.FC<FilterPopperProps>;
