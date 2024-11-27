4b3b8fdec99d2b2f33facb4229cff962605b7a6e
Petauskas, Áron <67595415+AronPetrauskas@users.noreply.github.com>
2024-11-27 09:19:35 +0100

    create `DateLabel` component, write tests

--- src/index.ts
+++ src/index.ts
@@ -38,6 +38,7 @@ export {
   type SummaryCardProps,
   type TableCardProps
 } from "./Card";
+export { DateLabel, type DateLabelProps } from "./DateLabel";
 export { FileLabel, type FileLabelProps } from "./FileLabel";
 export { VersionLabel, type VersionLabelProps } from "./VersionLabel";
 export { FormatLabel, type FormatLabelProps } from "./FormatLabel";
