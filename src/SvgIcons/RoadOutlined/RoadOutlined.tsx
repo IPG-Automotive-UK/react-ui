3ee14f504941c934d61121b2220fdcb21d660e52
Petauskas, Áron <67595415+AronPetrauskas@users.noreply.github.com>
2024-12-03 12:30:02 +0100

    fix color not changing

--- src/SvgIcons/RoadOutlined/RoadOutlined.tsx
+++ src/SvgIcons/RoadOutlined/RoadOutlined.tsx
@@ -4,40 +4,20 @@ import { SvgIcon } from "@mui/material";
 
 export default function RoadOutlinedIcon({ sx }: RoadOutlinedProps) {
   return (
-    <SvgIcon data-testid={"road-outlined-icon"} sx={sx}>
-      <svg
-        width="16"
-        height="16"
-        viewBox="0 0 16 16"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M15.4984 0.5H13.5073V15.5H15.4984V0.5Z"
-          fill="black"
-          fillOpacity="0.54"
-        />
-        <path
-          d="M2.35844 0.631836H0.5V15.3687H2.35844V0.631836Z"
-          fill="black"
-          fillOpacity="0.54"
-        />
-        <path
-          d="M8.86399 0.631836H7.00555V4.31605H8.86399V0.631836Z"
-          fill="black"
-          fillOpacity="0.54"
-        />
-        <path
-          d="M8.86399 6.15723H7.00555V9.84144H8.86399V6.15723Z"
-          fill="black"
-          fillOpacity="0.54"
-        />
-        <path
-          d="M8.86399 11.6846H7.00555V15.3688H8.86399V11.6846Z"
-          fill="black"
-          fillOpacity="0.54"
-        />
-      </svg>
+    <SvgIcon
+      data-testid={"road-outlined-icon"}
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="inherit"
+      xmlns="http://www.w3.org/2000/svg"
+      sx={{ ...sx }}
+    >
+      <path d="M15.4984 0.5H13.5073V15.5H15.4984V0.5Z" />
+      <path d="M2.35844 0.631836H0.5V15.3687H2.35844V0.631836Z" />
+      <path d="M8.86399 0.631836H7.00555V4.31605H8.86399V0.631836Z" />
+      <path d="M8.86399 6.15723H7.00555V9.84144H8.86399V6.15723Z" />
+      <path d="M8.86399 11.6846H7.00555V15.3688H8.86399V11.6846Z" />
     </SvgIcon>
   );
 }
