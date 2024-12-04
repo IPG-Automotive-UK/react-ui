cd78708dc5e577a983f15f24459807dc0fe44e92
Kaustubh Krishna Borpujari <kaustubh.krishna.borpujari@ipg-automotive.com>
2024-12-04 16:57:31 +0530

    Reverting changes to AlwaysOpenAutocomplete to rerun pipeline test

--- src/Filter/AlwaysOpenAutocomplete.tsx
+++ src/Filter/AlwaysOpenAutocomplete.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 
+import { Autocomplete, Stack } from "@mui/material";
+
 import type { AlwaysOpenAutocompleteProps } from "./AlwaysOpenAutocomplete.types";
-import { Stack } from "@mui/material";
-import { VirtualizedAutocomplete } from "./CheckboxFilter/VirtualizedAutocomplete";
 
 /**
  * An overriden Autocomplete component that is always open
@@ -13,7 +13,7 @@ export default function AlwaysOpenAutocomplete(
   // NOTE: The stack is required because the Autocomplete component inserts the "PopperComponent" as a sibling.
   return (
     <Stack direction="column">
-      <VirtualizedAutocomplete open disableCloseOnSelect {...props} />
+      <Autocomplete open disableCloseOnSelect {...props} />
     </Stack>
   );
 }
