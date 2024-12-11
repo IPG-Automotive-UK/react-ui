82d62907514d3f80f4e63b724b850b912cde0d0d
Petauskas, Áron <67595415+AronPetrauskas@users.noreply.github.com>
2024-12-11 15:19:56 +0100

    unify color list

--- src/UserAvatar/colorMap.tsx
+++ src/UserAvatar/colorMap.tsx
@@ -11,6 +11,18 @@ import {
 
 import { useMemo } from "react";
 
+// define list of colors
+const colors = [
+  indigo[500],
+  green[700],
+  deepOrange[500],
+  cyan[700],
+  pink[600],
+  teal[600],
+  purple[500],
+  red[600]
+];
+
 /**
  * Custom hook (with memo) to generate a color map for a list of options
  * @param allOptions A list of all values that should be assigned a color
@@ -18,18 +30,6 @@ import { useMemo } from "react";
 export default function useColorMap(allOptions: string[]) {
   // generate color map from options
   const colorMap = useMemo(() => {
-    // define list of colors
-    const colors = [
-      indigo[500],
-      green[700],
-      deepOrange[500],
-      cyan[700],
-      pink[600],
-      teal[600],
-      purple[500],
-      red[600]
-    ];
-
     // create color map
     const colorMap: { [key: string]: string } = {};
     for (let i = 0; i < allOptions.length; i++) {
@@ -48,18 +48,6 @@ export default function useColorMap(allOptions: string[]) {
  * @param allOptions A list of all values that should be assigned a color
  */
 export function colorMap(allOptions: string[]) {
-  // define list of colors
-  const colors = [
-    indigo[500],
-    green[700],
-    deepOrange[500],
-    cyan[700],
-    pink[600],
-    teal[600],
-    purple[500],
-    red[600]
-  ];
-
   // create color map
   const colorMap: { [key: string]: string } = {};
   for (let i = 0; i < allOptions.length; i++) {
