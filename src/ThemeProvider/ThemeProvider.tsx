e864322542eed1e2c248f28d8376d7c2258ed724
Manjesh M Pillai <m4manjesh@gmail.com>
2025-05-15 16:39:10 +0100

    Updated theme provider

--- src/ThemeProvider/ThemeProvider.tsx
+++ src/ThemeProvider/ThemeProvider.tsx
@@ -1,5 +1,11 @@
+/* eslint-disable no-undef */
+
+import type {} from "@mui/x-data-grid/themeAugmentation";
+import type {} from "@mui/material/themeCssVarsAugmentation";
+
 import {
   ThemeProvider as MuiThemeProvider,
+  alpha,
   createTheme,
   useColorScheme
 } from "@mui/material/styles";
@@ -7,6 +13,26 @@ import React, { useEffect } from "react";
 
 import { ThemeProviderProps } from "./ThemeProvider.types";
 import { darkScrollbar } from "@mui/material";
+
+// extend the theme to include custom properties
+// https://mui.com/material-ui/customization/theming/#custom-variables
+declare module "@mui/material/styles" {
+  // allow configuration using `createTheme`
+  // eslint-disable-next-line no-unused-vars
+
+  export type PlotlyColorScale = [number, string];
+
+  // eslint-disable-next-line no-unused-vars
+  interface Theme {
+    layout: {
+      content: {
+        maxWidth: number;
+      };
+    };
+
+    plotlyColorScales: PlotlyColorScale[];
+  }
+}
 
 // primary main light
 const primaryLightMain = "#3D5A75";
@@ -48,81 +74,226 @@ const paletteDefaultBackgroundPaperLight = "#FFFFFF";
 const paletteDefaultBackgroundPaperDark = "#182533";
 
 // palette tooltip color light mode or dark mode
-// const paletteTooltipColor = "#3C4F67";
+const paletteTooltipColor = "#3C4F67";
 
 // // datagrid dark mode border color
-// const dataGridDarkBorderColor = "#343F4B";
+const dataGridDarkBorderColor = "#343F4B";
 
-export const theme = createTheme({
-  colorSchemes: {
-    dark: {
-      palette: {
-        background: {
-          default: paletteDefaultBackgroundDark,
-          paper: paletteDefaultBackgroundPaperDark
-        },
-        primary: {
-          dark: primaryDarkDark,
-          light: primaryDarkLight,
-          main: primaryDarkMain
-        },
-        secondary: {
-          dark: secondaryColorDark,
-          light: secondaryColorLight,
-          main: secondaryColorMain
-        }
-      }
-    },
-    light: {
-      palette: {
-        background: {
-          default: paletteDefaultBackgroundLight,
-          paper: paletteDefaultBackgroundPaperLight
-        },
-        primary: {
-          dark: primaryLightDark,
-          light: primaryLightLight,
-          main: primaryLightMain
-        },
-        secondary: {
-          dark: secondaryColorDark,
-          light: secondaryColorLight,
-          main: secondaryColorMain
-        }
-      }
-    }
-  },
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: ({ theme }) => ({
-          "&.MuiButton-containedPrimary": {
-            "&.Mui-disabled": {
-              color: `${theme.vars.palette.text.disabled} !important`
-            },
-            color: theme.vars.palette.background.default
+export const theme = (() => {
+  const baseTheme = createTheme({
+    colorSchemes: {
+      dark: {
+        palette: {
+          background: {
+            default: paletteDefaultBackgroundDark,
+            paper: paletteDefaultBackgroundPaperDark
+          },
+          primary: {
+            dark: primaryDarkDark,
+            light: primaryDarkLight,
+            main: primaryDarkMain
+          },
+          secondary: {
+            dark: secondaryColorDark,
+            light: secondaryColorLight,
+            main: secondaryColorMain
           }
-        })
+        }
+      },
+      light: {
+        palette: {
+          background: {
+            default: paletteDefaultBackgroundLight,
+            paper: paletteDefaultBackgroundPaperLight
+          },
+          primary: {
+            dark: primaryLightDark,
+            light: primaryLightLight,
+            main: primaryLightMain
+          },
+          secondary: {
+            dark: secondaryColorDark,
+            light: secondaryColorLight,
+            main: secondaryColorMain
+          }
+        }
       }
     },
-    MuiCssBaseline: {
-      styleOverrides: {
-        body: {
-          ...darkScrollbar()
-        },
-        "html[data-color-scheme='light'] body": darkScrollbar({
-          active: "red",
-          thumb: "blue",
-          track: "green"
-        })
+    components: {
+      MuiAccordionSummary: {
+        styleOverrides: {
+          root: {
+            "&.Mui-expanded": {
+              marginBottom: -20
+            }
+          }
+        }
+      },
+      MuiAppBar: {
+        styleOverrides: {
+          root: {
+            "--Paper-overlay": "none !important",
+            backgroundColor: paletteDefaultBackgroundPaperLight
+          }
+        }
+      },
+      MuiAvatar: {
+        styleOverrides: {
+          root: {
+            fontFamily: "Montserrat, Arial, sans-serif"
+          }
+        }
+      },
+      MuiBadge: {
+        styleOverrides: {
+          badge: {
+            fontFamily: "Montserrat, Arial, sans-serif"
+          }
+        }
+      },
+      MuiButton: {
+        styleOverrides: {
+          root: ({ theme }) => ({
+            "&.MuiButton-containedPrimary": {
+              "&.Mui-disabled": {
+                color: `${theme.vars.palette.text.disabled} !important`
+              },
+              color: theme.vars.palette.background.default
+            }
+          })
+        }
+      },
+      MuiChip: {
+        styleOverrides: {
+          root: {
+            fontFamily: "Montserrat, Arial, sans-serif"
+          }
+        }
+      },
+      MuiCssBaseline: {
+        styleOverrides: {
+          body: {
+            ...darkScrollbar()
+          },
+          "html[data-color-scheme='light'] body": darkScrollbar({
+            active: `var(--ipg-palette-grey-400)`,
+            thumb: `var(--ipg-palette-grey-400)`,
+            track: `var(--ipg-palette-grey-200)`
+          })
+        }
+      },
+      MuiDataGrid: {
+        styleOverrides: {
+          footerContainer: ({ theme }) => ({
+            backgroundColor: theme.vars.palette.background.paper
+          }),
+          main: ({ theme }) => ({
+            backgroundColor: theme.vars.palette.background.paper
+          }),
+          root: ({ theme }) => ({
+            "& .MuiDataGrid-cell:focus, .MuiDataGrid-cell:focus-within, .MuiDataGrid-columnHeader:focus, .MuiDataGrid-columnHeader:focus-within":
+              {
+                outline: "none"
+              },
+            "& .MuiDataGrid-columnSeparator": {
+              color: theme.vars.palette.divider
+            },
+            "& .MuiDataGrid-container--top [role='row'], & .MuiDataGrid-container--bottom [role='row']":
+              {
+                backgroundColor: theme.vars.palette.background.paper
+              },
+            "& .MuiDataGrid-withBorderColor": {
+              borderColor: theme.vars.palette.divider
+            },
+            "--DataGrid-rowBorderColor": theme.vars.palette.divider,
+            // If the theme is dark, set the border color to the dataGridDarkBorderColor
+            ...(theme.palette.mode === "dark" && {
+              borderColor: dataGridDarkBorderColor
+            })
+          })
+        }
+      },
+      MuiFormLabel: {
+        styleOverrides: {
+          asterisk: ({ theme }) => ({
+            color: theme.vars.palette.error.main
+          })
+        }
+      },
+      MuiStepIcon: {
+        styleOverrides: {
+          text: {
+            fontFamily: "Montserrat, Arial, sans-serif"
+          }
+        }
+      },
+      MuiStepper: {
+        styleOverrides: {
+          root: ({ theme }) => ({
+            backgroundColor: alpha(theme.vars.palette.primary.main, 0.08)
+          })
+        }
+      },
+      MuiTableCell: {
+        styleOverrides: {
+          root: () => ({
+            "&.MuiTableCell-head": {
+              backgroundColor: paletteDefaultBackgroundPaperDark
+            }
+          })
+        }
+      },
+      MuiTableRow: {
+        styleOverrides: {
+          root: ({ theme }) => ({
+            "&.Mui-selected": {
+              backgroundColor: alpha(theme.vars.palette.primary.main, 0.08)
+            }
+          })
+        }
+      },
+      MuiTooltip: {
+        styleOverrides: {
+          tooltip: {
+            background: paletteTooltipColor,
+            fontFamily: "Montserrat, Arial, sans-serif",
+            fontSize: "12px",
+            fontWeight: 400
+          }
+        }
+      }
+    },
+    cssVariables: {
+      colorSchemeSelector: "data",
+      cssVarPrefix: "ipg"
+    },
+    mixins: {
+      toolbar: {
+        minHeight: 64
+      }
+    },
+    typography: {
+      allVariants: {
+        fontFamily: "Montserrat, Arial, sans-serif"
       }
     }
-  },
-  cssVariables: {
-    colorSchemeSelector: "data",
-    cssVarPrefix: "ipg"
-  }
-});
+  });
+
+  // Add custom properties after theme creation
+  return {
+    ...baseTheme,
+    layout: {
+      content: {
+        maxWidth: 1152
+      }
+    },
+    plotlyColorScales: [
+      [0, "#3D5A75"],
+      [0.5, "#FFFFFF"],
+      [1, "#FFAF2C"]
+    ]
+  };
+})();
 
 export default function ThemeProvider({
   children,
@@ -151,12 +322,6 @@ function ControlledThemeWrapper({
       setMode(controlledTheme);
     }
 
-    if (controlledTheme) {
-      document.documentElement.setAttribute(
-        "data-color-scheme",
-        controlledTheme
-      );
-    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [controlledTheme]);
 
